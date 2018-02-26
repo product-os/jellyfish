9b4328cb083c9112dab66d1beb351fd0f57f64b0
Juan Cruz Viotti <jv@jviotti.com>
2018-02-26 12:32:36 -0400

    Remove engines from package.json
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- components/server.js
+++ components/server.js
@@ -23,6 +23,6 @@ app.get('/', (request, response) => {
   response.send('Hello World!')
 })
 
-app.listen(80, () => {
-  console.log('Example app listening on port 80!')
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
 })
