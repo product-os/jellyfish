7e21618ac30a2d13f18cf1ebab01479aa1cb51e6
Juan Cruz Viotti <jv@jviotti.com>
2018-02-26 12:34:14 -0400

    Update listen port
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- components/server.js
+++ components/server.js
@@ -23,6 +23,6 @@ app.get('/', (request, response) => {
   response.send('Hello World!')
 })
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+app.listen(80, () => {
+  console.log('Example app listening on port 80!')
 })
