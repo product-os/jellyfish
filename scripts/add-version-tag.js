46a530c733581523d03d998a546a9d5276b460f3
Lucian Buzzo <lucian.buzzo@gmail.com>
2018-06-07 16:13:39 +0100

    Add script for adding a new version tag

---
+++ scripts/add-version-tag.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const util = require('util')
+const _ = require('lodash')
+const version = require('../package.json').version
+const exec = util.promisify(require('child_process').exec)
+
+const checkGitTag = async (tag) => {
+	const {
+		stdout,
+		stderr
+	} = await exec('git tag')
+
+	if (stderr) {
+		throw new Error(stderr)
+	}
+
+	if (_.includes(stdout, `v${tag}`)) {
+		throw new Error(`Git tag v${tag} already exists: aborting`)
+	}
+}
+
+const setGitTag = async (tag) => {
+	const {
+		stderr
+	} = await exec(`git tag -a v${tag} -m "v${tag}"`)
+
+	if (stderr) {
+		throw new Error(stderr)
+	}
+}
+
+const run = async () => {
+	try {
+		await checkGitTag(version)
+		await setGitTag(version)
+		console.log(`Added git tag v${version}`)
+	} catch (error) {
+		console.error(error.message)
+	}
+}
+
+run()
