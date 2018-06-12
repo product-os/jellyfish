a5fb360567e68afbcf9187ee0803cd968ab4784e
Lucian Buzzo <lucian.buzzo@gmail.com>
2018-06-12 12:21:08 +0100

    Add license information and JSDoc comments to SDK
    change-type: patch

--- lib/sdk/card.ts
+++ lib/sdk/card.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import * as Bluebird from 'bluebird';
 import { JSONSchema6 } from 'json-schema';
 import * as _ from 'lodash';
@@ -7,6 +23,31 @@ import { debug, isUUID, SDKInterface } from './utils';
 export class CardSdk {
 	constructor(private sdk: SDKInterface) {}
 
+	/**
+	 * @summary Get a card
+	 * @name get
+	 * @public
+	 * @function
+	 * @memberof jellyfishSdk.card
+	 *
+	 * @description Get a card using an id or a slug
+	 *
+	 * @param {String} idOrSlug - The id or slug of the card to retrieve
+	 *
+	 * @fulfil {Object|null} - A single card, or null if one wasn't found
+	 * @returns {Promise}
+	 *
+	 * @example
+	 * jellyfishSdk.card.get('user-johndoe')
+	 * 	.then((card) => {
+	 * 		console.log(card)
+	 * 	})
+	 *
+	 * jellyfishSdk.card.get('8b465c9a-b4cb-44c1-9df9-632649d7c4c3')
+	 * 	.then((card) => {
+	 * 		console.log(card)
+	 * 	})
+	 */
 	public get(idOrSlug: string): Bluebird<Card | null> {
 		debug(`Fetching card ${idOrSlug}`);
 
@@ -38,7 +79,24 @@ export class CardSdk {
 	}
 
 	/**
-	 * @summary Get all cards of a given type
+	 * @summary Get a all cards of a given type
+	 * @name getAllByType
+	 * @public
+	 * @function
+	 * @memberof jellyfishSdk.card
+	 *
+	 * @description Get all cards that have the provided 'type' attribute
+	 *
+	 * @param {String} type - The type of card to retrieve
+	 *
+	 * @fulfil {Object[]} - All cards of the given type
+	 * @returns {Promise}
+	 *
+	 * @example
+	 * jellyfishSdk.card.getAllByType('view')
+	 * 	.then((cards) => {
+	 * 		console.log(cards)
+	 * 	})
 	 */
 	public getAllByType(cardType: string): Bluebird<Card[]> {
 		const schema: JSONSchema6 = {
@@ -56,6 +114,27 @@ export class CardSdk {
 		return this.sdk.query(schema);
 	}
 
+	/**
+	 * @summary Get the timeline for a card
+	 * @name getTimeline
+	 * @public
+	 * @function
+	 * @memberof jellyfishSdk.card
+	 *
+	 * @description Get all the timeline cards that target a card with the
+	 * specified id
+	 *
+	 * @param {String} id - The id of the card to retrieve a timeline for
+	 *
+	 * @fulfil {Object[]} - A set of timeline cards
+	 * @returns {Promise}
+	 *
+	 * @example
+	 * jellyfishSdk.card.getTimeline('8b465c9a-b4cb-44c1-9df9-632649d7c4c3')
+	 * 	.then((timeline) => {
+	 * 		console.log(timeline)
+	 * 	})
+	 */
 	public getTimeline(id: string): Bluebird<Card[]> {
 		const schema: JSONSchema6 = {
 			type: 'object',
@@ -85,7 +164,30 @@ export class CardSdk {
 	}
 
 	/**
-	 * Resolves with the ID of the created card
+	 * @summary Create a new card
+	 * @name create
+	 * @public
+	 * @function
+	 * @memberof jellyfishSdk.card
+	 *
+	 * @description Send an action request to create a new card
+	 *
+	 * @param {Object} card - The card that should be created, must include
+	 * a 'type' attribute.
+	 *
+	 * @fulfil {String} - The id of the newly created card
+	 * @returns {Promise}
+	 *
+	 * @example
+	 * jellyfishSdk.card.create({
+	 * 	type: 'thread',
+	 * 	data: {
+	 * 		description: 'lorem ipsum dolor sit amet'
+	 * 	}
+	 * })
+	 * 	.then((id) => {
+	 * 		console.log(id)
+	 * 	})
 	 */
 	public create(card: Partial<Card> & { type: string }): Bluebird<string> {
 		return this.sdk.action<string>({
@@ -97,6 +199,31 @@ export class CardSdk {
 		});
 	}
 
+	/**
+	 * @summary Update a card
+	 * @name update
+	 * @public
+	 * @function
+	 * @memberof jellyfishSdk.card
+	 *
+	 * @description Send an action request to update a card
+	 *
+	 * @param {String} id - The id of the card that should be updated
+	 * @param {Object} body - An object that will be used to update the card
+	 *
+	 * @fulfil {Object} - An action response object
+	 * @returns {Promise}
+	 *
+	 * @example
+	 * jellyfishSdk.card.update('8b465c9a-b4cb-44c1-9df9-632649d7c4c3', {
+	 * 	data: {
+	 * 		description: 'foo bar baz'
+	 * 	}
+	 * })
+	 * 	.then((response) => {
+	 * 		console.log(response)
+	 * 	})
+	 */
 	public update(id: string, body: Partial<Card>) {
 		return this.sdk.action({
 			target: id,
@@ -107,6 +234,22 @@ export class CardSdk {
 		});
 	}
 
+	/**
+	 * @summary Remove a card
+	 * @name remove
+	 * @public
+	 * @function
+	 * @memberof jellyfishSdk.card
+	 *
+	 * @description Send an action request to remove a card
+	 *
+	 * @param {String} id - The id of the card that should be remove
+	 *
+	 * @returns {Promise}
+	 *
+	 * @example
+	 * jellyfishSdk.card.remove('8b465c9a-b4cb-44c1-9df9-632649d7c4c3')
+	 */
 	public remove(id: string) {
 		return this.sdk.action({
 			target: id,
--- lib/sdk/utils.ts
+++ lib/sdk/utils.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 resin.io
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
 import * as Promise from 'bluebird';
 import { JSONSchema6 } from 'json-schema';
@@ -21,6 +37,21 @@ const DEBUG =	!_.includes([
 	'test',
 ], process.env.NODE_ENV);
 
+/**
+ * @summary Print a debug message to the console
+ * @name debug
+ * @public
+ * @function
+ * @memberof jellyfishSdk.utils
+ *
+ * @description Stream updates and insertions for cards that match a JSON
+ * schema
+ *
+ * @param {*} params - The data to print to the console
+ *
+ * @example
+ * debug('foo bar baz')
+ */
 export const debug = (...params: any[]) => {
 	if (DEBUG) {
 		console.log('%cjellyfish:sdk', `color: ${ORANGE};`, ...params);
@@ -29,8 +60,10 @@ export const debug = (...params: any[]) => {
 
 /**
  * @summary Check if a string is a UUID
+ * @name isUUID
  * @function
  * @public
+ * @memberof jellyfishSdk.utils
  *
  * @param {String} string - string
  * @returns {Boolean} whether the string is a uuid
@@ -44,10 +77,52 @@ export const isUUID = (text: string) => {
 	return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(text);
 };
 
+/**
+ * @summary Convert a string into a value that can be used as a slug
+ * @name slugify
+ * @function
+ * @public
+ * @memberof jellyfishSdk.utils
+ *
+ * @description Lowercases text, then converts spaces to hyphens and removes any character that isn't
+ * alphanumeric or a dash
+ *
+ * @param {String} string - string
+ * @returns {String} A valid slug
+ *
+ * @example
+ * const slug = utils.slugify('Lorem ipsum!')
+ * console.log(slug) //--> 'lorem-ipsum'
+ */
 export const slugify = (text: string) => text.toLowerCase()
 	.replace(/\s+/g, '-')
 	.replace(/[^0-9a-z-]/g, '');
 
+/**
+ * @summary Compile a schema using AJV
+ * @name compileSchema
+ * @function
+ * @public
+ * @memberof jellyfishSdk.utils
+ *
+ * @description Compiles a schema using AJV, return a validator function
+ * @see https://github.com/epoberezkin/ajv#compileobject-schema---functionobject-data
+ *
+ * @param {Object} schema - A JSON schema
+ * @returns {Function} An ajv validator function
+ *
+ * @example
+ * const schema = {
+ * 	type: 'object',
+ * 	properies: {
+ * 		type: {
+ * 			const: 'thread'
+ * 		}
+ * 	}
+ * };
+ *
+ * const validator = utils.compileSchema(schema);
+ */
 export const compileSchema = (schema: JSONSchema6) => ajv.compile(schema);
 
 export interface ActionResponse {
