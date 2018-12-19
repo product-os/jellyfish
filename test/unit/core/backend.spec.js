0a397f1fea0ee863bd3829931dd662f33a7c7c06
Juan Cruz Viotti <juan@balena.io>
2018-12-19 16:15:00 +0000

    core: Always cap the amount of elements a full query can return
    Change-type: major
    Signed-off-by: Juan Cruz Viotti <juan@balena.io>

--- test/unit/core/backend.spec.js
+++ test/unit/core/backend.spec.js
@@ -846,6 +846,22 @@ ava('.query() should be able to limit the results', async (test) => {
 	test.deepEqual(_.sortBy(results, [ 'test' ]), [ result1, result2 ])
 })
 
+ava('.query() should throw given an invalid limit', async (test) => {
+	await test.throwsAsync(test.context.backend.query({
+		type: 'object',
+		additionalProperties: true,
+		properties: {
+			type: {
+				type: 'string',
+				const: 'card'
+			}
+		},
+		required: [ 'type' ]
+	}, {
+		limit: 101
+	}), errors.JellyfishInvalidLimit)
+})
+
 ava('.query() should be able to skip the results', async (test) => {
 	await test.context.backend.upsertElement({
 		type: 'card',
