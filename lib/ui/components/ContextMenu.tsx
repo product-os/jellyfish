2c09f39ff894cbf1fc2033f041cb21f3e6d5a890
Lucian <lucian.buzzo@gmail.com>
2019-01-07 11:31:24 +0000

    UI: Improve positioning of context menu
    Change-type: patch
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- lib/ui/components/ContextMenu.tsx
+++ lib/ui/components/ContextMenu.tsx
@@ -6,6 +6,9 @@ import {
 } from 'rendition';
 import styled from 'styled-components';
 
+const ARROW_WIDTH = 7;
+const ARROW_RIGHT_OFFSET = 11;
+
 const Menu = styled(Box)`
 	background-clip: padding-box;
 	background-color: #fff;
@@ -44,34 +47,34 @@ const Menu = styled(Box)`
 		&:before {
 			left: -6px;
 			top: 9px;
-			border-top: 7px solid transparent;
-			border-bottom: 7px solid transparent;
-			border-right: 6px solid #ccc;
+			border-top: ${ARROW_WIDTH}px solid transparent;
+			border-bottom: ${ARROW_WIDTH}px solid transparent;
+			border-right: ${ARROW_WIDTH - 1}px solid #ccc;
 		}
 
 		&:after {
 			left: -5px;
 			top: 10px;
-			border-top: 6px solid transparent;
-			border-bottom: 6px solid transparent;
-			border-right: 5px solid #fff;
+			border-top: ${ARROW_WIDTH - 1}px solid transparent;
+			border-bottom: ${ARROW_WIDTH - 1}px solid transparent;
+			border-right: ${ARROW_WIDTH - 2}px solid #fff;
 		}
 	}
 
 	&.context-menu--bottom {
 		&:before {
-			right: 11px;
+			right: ${ARROW_RIGHT_OFFSET}px;
 			top: -6px;
-			border-right: 7px solid transparent;
-			border-left: 7px solid transparent;
-			border-bottom: 6px solid #ccc;
+			border-right: ${ARROW_WIDTH}px solid transparent;
+			border-left: ${ARROW_WIDTH}px solid transparent;
+			border-bottom: ${ARROW_WIDTH - 1}px solid #ccc;
 		}
 
 		&:after {
-			right: 12px;
+			right: ${ARROW_RIGHT_OFFSET + 1}px;
 			top: -5px;
-			border-right: 6px solid transparent;
-			border-left: 6px solid transparent;
+			border-right: ${ARROW_WIDTH - 1}px solid transparent;
+			border-left: ${ARROW_WIDTH - 1}px solid transparent;
 			border-bottom: 5px solid #fff;
 		}
 	}
@@ -113,7 +116,7 @@ export class ContextMenu extends React.Component<ContextMenuProps, ContextMenuSt
 			this.setState({
 				offsetLeft: 'auto',
 				offsetTop: bounds.top + bounds.height,
-				offsetRight: window.innerWidth - (bounds.left + bounds.width),
+				offsetRight: window.innerWidth - (bounds.left + bounds.width / 2 + ARROW_WIDTH / 2 + ARROW_RIGHT_OFFSET + 4),
 			});
 		} else {
 			this.setState({
