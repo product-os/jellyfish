cb6150d34e1abadbfbd3f494f5f057d63081db1d
Lucian <lucian.buzzo@gmail.com>
2019-01-15 16:10:51 +0000

    UI: Safely handle uncaught errors when rendering a channel
    Change-type: minor
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- lib/ui/index.tsx
+++ lib/ui/index.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Theme } from 'rendition';
 import { injectGlobal } from 'styled-components';
+import { ErrorBoundary } from './components/ErrorBoundary';
 import { store } from './core';
 import { JellyfishUI } from './JellyfishUI';
 
@@ -39,7 +40,9 @@ if (process.env.NODE_ENV === 'production' && SENTRY_DSN) {
 
 ReactDOM.render(
 	<Provider store={store}>
-		<JellyfishUI />
+		<ErrorBoundary>
+			<JellyfishUI />
+		</ErrorBoundary>
 	</Provider>,
 	document.getElementById('app'),
 );
