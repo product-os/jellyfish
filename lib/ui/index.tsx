d28fcea3e0a11e47e3cafa82b2635bc95740f0a9
Lucian <lucian.buzzo@gmail.com>
2019-01-15 09:38:38 +0000

    UI: Use injectGlobal to set global styles and add line-height rule
    The line-height change improves readability of messages.
    
    Change-type: minor
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- lib/ui/index.tsx
+++ lib/ui/index.tsx
@@ -2,8 +2,29 @@ import * as Sentry from '@sentry/browser';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Theme } from 'rendition';
+import { injectGlobal } from 'styled-components';
 import { store } from './core';
 import { JellyfishUI } from './JellyfishUI';
+
+injectGlobal`
+  * {
+    boxSizing: border-box;
+  }
+
+  body {
+    line-height: 1.5;
+    margin: 0;
+    font-family: ${Theme.font};
+  }
+
+	html,
+	body,
+	#app {
+		height: 100%;
+	}
+`;
+
 
 const SENTRY_DSN = process.env.SENTRY_DSN_UI;
 
