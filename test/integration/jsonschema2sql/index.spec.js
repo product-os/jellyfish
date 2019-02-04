d8a3fbf619f064d73dfd80fa412757763132f066
Juan Cruz Viotti <juan@balena.io>
2019-02-04 16:31:30 -0400

    jsonschema2sql: Refactor test suite a bit
    Change-type: patch
    Signed-off-by: Juan Cruz Viotti <juan@balena.io>

--- test/integration/jsonschema2sql/index.spec.js
+++ test/integration/jsonschema2sql/index.spec.js
@@ -4,46 +4,64 @@
  * Proprietary and confidential.
  */
 
-const _ = require('lodash')
 const ava = require('ava')
 const jsonSchemaTestSuite = require('json-schema-test-suite')
 const uuid = require('uuid/v4')
 const pgp = require('pg-promise')()
 const format = require('pg-format').literal
 const environment = require('../../../lib/environment')
-const translator = require('../../../lib/jsonschema2sql')
+const jsonschema2sql = require('../../../lib/jsonschema2sql')
 
+/*
+ * The list of JSON Schema suites we support. This
+ * list represents the JSON Schema keywords that are
+ * safe to use in queries.
+ *
+ * See https://github.com/json-schema-org/JSON-Schema-Test-Suite/tree/master/tests
+ * for more details.
+ */
+/* eslint-disable capitalized-comments, lines-around-comment */
 const SUPPORTED_SUITES = [
+	// additionalItems
+	// additionalProperties
 	'allOf',
 	'anyOf',
+	// boolean_schema
 	'const',
+	// contains
 	'default',
+	// definitions
+	// dependencies
 	'enum',
+	// exclusiveMaximum
+	// exclusiveMinimum
+	// items
+	// maxItems
 	'maxLength',
+	// maxProperties
 	'maximum',
+	// minItems
 	'minLength',
+	// minProperties
 	'minimum',
+	// multipleOf
 	'not',
+	// oneOf
+	// bignum
+	// ecmascript-regex
+	// format
+	// zeroTerminatedFloats
 	'pattern',
 	'patternProperties',
+	// properties
+	// propertyNames
+	// ref
+	// refRemote
 	'required',
 	'type'
+	// uniqueItems
 ]
-
-const suites = jsonSchemaTestSuite.draft6()
-
-const createTable = async (connection, table) => {
-	await connection.any(`
-		CREATE TABLE IF NOT EXISTS ${table} (
-			id SERIAL PRIMARY KEY,
-			card_data jsonb
-		)
-	`)
-}
-
-const context = {
-	database: `test_${uuid().replace(/-/g, '')}`
-}
+/* eslint-enable capitalized-comments, lines-around-comment */
 
 const runner = async ({
 	connection,
@@ -52,91 +70,173 @@ const runner = async ({
 	schema,
 	table
 }) => {
-	await createTable(connection, table)
+	/*
+	 * 1. Create a unique table for the test.
+	 */
+	await connection.any(`CREATE TABLE IF NOT EXISTS ${table} (
+		id SERIAL PRIMARY KEY,
+		card_data jsonb
+	)`)
 
+	/*
+	 * 2. Insert the elements we will try to query.
+	 */
 	for (const item of elements) {
 		await connection.any(
-			`INSERT INTO ${table} (card_data) VALUES (${format(JSON.stringify(item))})`
-		)
+			`INSERT INTO ${table} (card_data) VALUES (${format(JSON.stringify(item))})`)
 	}
 
-	const query = translator(table, schema, options)
+	/*
+	 * 3. Query the elements back using our translator.
+	 */
+	const query = jsonschema2sql(table, schema, options)
 
-	const results = await connection.any(query)
-
-	return results
+	/*
+	 * 4. Return the results.
+	 */
+	return connection.any(query)
 }
 
-ava.before(async () => {
+ava.before(async (test) => {
+	/*
+	 * Create a randomly generated database name where all the
+	 * test tables will be scoped. This ensures that can repeately
+	 * run the test suite without conflicts.
+	 */
+	test.context.database = `test_${uuid().replace(/-/g, '')}`
+
+	/*
+	 * Connect to the default postgres database and use that
+	 * connection to create the randomly generated database.
+	 */
 	await pgp({
 		user: environment.postgres.user,
 		password: environment.postgres.password,
 		database: 'postgres',
 		port: environment.postgres.port
 	}).any(`
-		CREATE DATABASE ${context.database} OWNER = ${environment.postgres.user};
+		CREATE DATABASE ${test.context.database} OWNER = ${environment.postgres.user};
 	`)
 
-	context.connection = pgp({
+	/*
+	 * Now that the auto-generated database is created, we
+	 * can connect to it, and store the connection in the context
+	 * so we can use it for queries and insertions.
+	 */
+	test.context.connection = await pgp({
 		user: environment.postgres.user,
-		database: context.database,
+		database: test.context.database,
 		password: environment.postgres.password,
 		port: environment.postgres.port
 	})
 })
 
-suites.forEach(async (suite) => {
-	suite.schemas.forEach((testSet, setIndex) => {
-		testSet.tests.forEach((testCase, caseIndex) => {
-			const title = `${suite.name}: ${testSet.description} - ${testCase.description} (${setIndex}_${caseIndex})`
-			const supported = _.includes(SUPPORTED_SUITES, suite.name)
-			const cmd = supported ? ava : ava.skip
-			cmd(title, async (test) => {
-				const table = `${suite.name}_${setIndex}_${caseIndex}`.replace(/[^0-9a-z_]/gi, '')
+/*
+ * The JSON Schema tests are divided in suites, where
+ * each of them corresponds to a JSON Schema keyword.
+ */
+for (const suite of jsonSchemaTestSuite.draft6()) {
+	/*
+	 * Each suite is then divided in scenarios, which
+	 * describe an object, along with a series of schemas
+	 * that may or may not match the object.
+	 */
+	for (const scenario of suite.schemas) {
+		/*
+		 * Each test case in an scenario contains a boolean
+		 * flag to determine whether it should match or
+		 * not the scenario's object.
+		 */
+		for (const testCase of scenario.tests) {
+			/*
+			 * We will execute each test case in a different
+			 * table.
+			 */
+			const table = [
+				suite.name,
+				suite.schemas.indexOf(scenario),
+				scenario.tests.indexOf(testCase)
+			].join('_').replace(/[^0-9a-z_]/gi, '')
 
+			/*
+			 * A readable title for the Ava test case
+			 */
+			const title = [
+				`${table}:`,
+				scenario.description,
+				'-',
+				testCase.description
+			].join(' ')
+
+			/*
+			 * Skip this test case if we don't support that test suite.
+			 * We could have omitted it from the suites list in the
+			 * first place, but this is a nice way to measure how far
+			 * we are from supporting the whole set of tests.
+			 */
+			const avaTest = SUPPORTED_SUITES.includes(suite.name)
+				? ava : ava.skip
+
+			/*
+			 * Run the test without any modification. We will insert
+			 * the scenario object, query it back with the test case
+			 * schema, and we expect to get a result of the schema
+			 * is expected to match.
+			 */
+			avaTest(`${title} [Normal]`, async (test) => {
 				const results = await runner({
-					connection: context.connection,
+					connection: test.context.connection,
 					table,
 					elements: [ testCase.data ],
-					schema: testSet.schema
+					schema: scenario.schema
 				})
 
 				test.is(results.length === 1, testCase.valid)
-			})
-
-			// Test that all the schema suite tests pass when the data and schema are nested
-			// inside an object
-			cmd(`[Nested object] ${title}`, async (test) => {
-				const table = `NO_${suite.name}_${setIndex}_${caseIndex}`.replace(/[^0-9a-z_]/gi, '')
-
-				const schema = {
-					type: 'object',
-					properties: {
-						wrapper: testSet.schema
-					}
+				if (testCase.valid) {
+					test.deepEqual(results[0].card_data, testCase.data)
 				}
+			})
 
-				const elements = [
-					{
-						wrapper: testCase.data
-					}
-				]
-
+			/*
+			 * Pretty much the same as before, but wrap the scenario
+			 * object into another object and wrap the schema
+			 * accordingly.
+			 */
+			avaTest(`${title} [Nested object]`, async (test) => {
 				const results = await runner({
-					connection: context.connection,
-					table,
-					elements,
-					schema
+					connection: test.context.connection,
+					table: `NESTED_${table}`,
+					elements: [
+						{
+							wrapper: testCase.data
+						}
+					],
+					schema: {
+						type: 'object',
+						properties: {
+							wrapper: scenario.schema
+						}
+					}
 				})
 
 				test.is(results.length === 1, testCase.valid)
+				if (testCase.valid) {
+					test.deepEqual(results[0].card_data, {
+						wrapper: testCase.data
+					})
+				}
 			})
-		})
-	})
-})
+		}
+	}
+}
+
+/*
+ * Some extra tests not covered by the official JSON Schema test suite.
+ */
 
 ava('injection - should escape malicious query keys', async (test) => {
 	const table = 'malicious_queries_0'
+
 	const schema = {
 		type: 'object',
 		properties: {
@@ -151,6 +251,7 @@ ava('injection - should escape malicious query keys', async (test) => {
 			}
 		}
 	}
+
 	const elements = [
 		{
 			'Robert\'); DROP TABLE cards; --': {
@@ -160,17 +261,18 @@ ava('injection - should escape malicious query keys', async (test) => {
 	]
 
 	const results = await runner({
-		connection: context.connection,
+		connection: test.context.connection,
 		table,
 		elements,
 		schema
 	})
 
-	test.true(results.length === 1)
+	test.is(results.length, 1)
 })
 
 ava('injection - should escape malicious query values', async (test) => {
 	const table = 'malicious_queries_1'
+
 	const schema = {
 		type: 'object',
 		properties: {
@@ -181,6 +283,7 @@ ava('injection - should escape malicious query values', async (test) => {
 		},
 		required: [ 'foo' ]
 	}
+
 	const elements = [
 		{
 			foo: 'Robert\'; DROP TABLE cards; --'
@@ -188,13 +291,13 @@ ava('injection - should escape malicious query values', async (test) => {
 	]
 
 	const results = await runner({
-		connection: context.connection,
+		connection: test.context.connection,
 		table,
 		elements,
 		schema
 	})
 
-	test.true(results.length === 1)
+	test.is(results.length, 1)
 })
 
 ava('order - should sort values in ascending order by default when specifying "sortBy"', async (test) => {
@@ -230,7 +333,7 @@ ava('order - should sort values in ascending order by default when specifying "s
 	]
 
 	const results = await runner({
-		connection: context.connection,
+		connection: test.context.connection,
 		table,
 		elements,
 		schema,
@@ -269,6 +372,7 @@ ava('order - should sort values in ascending order by default when specifying "s
 
 ava('order - should be able to sort values in descending order', async (test) => {
 	const table = 'order_1'
+
 	const schema = {
 		type: 'object',
 		properties: {
@@ -278,6 +382,7 @@ ava('order - should be able to sort values in descending order', async (test) =>
 		},
 		required: [ 'slug' ]
 	}
+
 	const elements = [
 		{
 			slug: 'beta',
@@ -300,7 +405,7 @@ ava('order - should be able to sort values in descending order', async (test) =>
 	]
 
 	const results = await runner({
-		connection: context.connection,
+		connection: test.context.connection,
 		table,
 		elements,
 		schema,
@@ -340,6 +445,7 @@ ava('order - should be able to sort values in descending order', async (test) =>
 
 ava('order - should be able to sort values by a single string value', async (test) => {
 	const table = 'order_2'
+
 	const schema = {
 		type: 'object',
 		properties: {
@@ -349,6 +455,7 @@ ava('order - should be able to sort values by a single string value', async (tes
 		},
 		required: [ 'slug' ]
 	}
+
 	const elements = [
 		{
 			slug: 'beta',
@@ -371,7 +478,7 @@ ava('order - should be able to sort values by a single string value', async (tes
 	]
 
 	const results = await runner({
-		connection: context.connection,
+		connection: test.context.connection,
 		table,
 		elements,
 		schema,
