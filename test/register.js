64241df04ea805487183721e6e85ea4ca1e55134
Lucian <lucian.buzzo@gmail.com>
2019-03-07 15:59:49 +0000

    Only use babel register for JSX files
    Change-type: patch
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>
    Signed-off-by: Juan Cruz Viotti <juan@balena.io>

---
+++ test/register.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+// A helper that limits the files that are transpiled
+// using babel during Ava's test step.
+// See https://github.com/avajs/ava/blob/master/docs/recipes/babel.md#compile-sources
+
+require('@babel/register')({
+	extensions: [ '.jsx' ]
+})
