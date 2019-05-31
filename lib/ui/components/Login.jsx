5ed488fafdef147338956dff93560d5c1aae0487
Lucian <lucian.buzzo@gmail.com>
2019-05-31 10:51:31 +0100

    UI: Submit login form when pressin the Enter key
    Fixes #2032
    
    Change-type: patch
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- lib/ui/components/Login.jsx
+++ lib/ui/components/Login.jsx
@@ -21,28 +21,6 @@ class Base extends React.Component {
 	constructor (props) {
 		super(props)
 
-		this.signup = (event) => {
-			event.preventDefault()
-			const {
-				username, password, email
-			} = this.state
-			this.setState({
-				loggingIn: true
-			})
-			Bluebird.try(() => {
-				return this.props.actions.signup({
-					username,
-					password,
-					email
-				})
-			})
-				.catch((error) => {
-					this.setState({
-						loggingIn: false
-					})
-					this.props.actions.addNotification('danger', error.message || error)
-				})
-		}
 		this.login = (event) => {
 			event.preventDefault()
 			const {
@@ -63,11 +41,6 @@ class Base extends React.Component {
 					})
 					this.props.actions.addNotification('danger', error.message || error)
 				})
-		}
-		this.toggleSignup = () => {
-			this.setState({
-				showSignup: !this.state.showSignup
-			})
 		}
 		this.togglePasswordVisibility = (event) => {
 			event.preventDefault()
@@ -96,7 +69,6 @@ class Base extends React.Component {
 			})
 		}
 		this.state = {
-			showSignup: false,
 			username: '',
 			password: '',
 			passwordConfirmation: '',
@@ -107,183 +79,82 @@ class Base extends React.Component {
 	}
 	render () {
 		const {
-			email, username, password, passwordConfirmation, loggingIn
+			username, password, loggingIn
 		} = this.state
-		const signupDisabled = !email || !username || !password || loggingIn || password !== passwordConfirmation
-		return (<React.Fragment>
-			<TopBar.default>
-				<rendition.Img
-					width={70}
-					style={{
-						height: 70
-					}}
-					pl={2}
-					p={10}
-					src="/icons/jellyfish.svg"
-				/>
-			</TopBar.default>
 
-			<rendition.Container mt={4} className="login-page">
-				<rendition.Box className={this.state.showSignup ? 'login-page__signup' : 'login-page__login'} mx="auto" style={{
-					maxWidth: 470
-				}}>
-					{this.state.showSignup && (
-						<React.Fragment>
-							<rendition.Txt align="center" mb={4}>
-								<rendition.Heading.h2 mb={2}>Sign up to Jellyfish</rendition.Heading.h2>
-								<span>Enter your details below</span>
-							</rendition.Txt>
+		return (
+			<React.Fragment>
+				<TopBar.default>
+					<rendition.Img
+						width={70}
+						style={{
+							height: 70
+						}}
+						pl={2}
+						p={10}
+						src="/icons/jellyfish.svg"
+					/>
+				</TopBar.default>
 
-							<rendition.Divider color="#eee" mb={4}/>
+				<rendition.Container mt={4} className="login-page">
+					<rendition.Box className="login-page__login" mx="auto" style={{
+						maxWidth: 470
+					}}>
+						<rendition.Txt align="center" mb={4}>
+							<rendition.Heading.h2 mb={2}>Log in to Jellyfish</rendition.Heading.h2>
+							<span>Enter your details below</span>
+						</rendition.Txt>
 
-							<form onSubmit={this.signup}>
-								<rendition.Txt fontSize={1} mb={1}>Email</rendition.Txt>
-								<rendition.Input
-									className="login-page__input--email"
-									mb={5}
-									w="100%"
-									emphasized={true}
-									type="email"
-									placeholder="Email"
-									value={this.state.email}
-									onChange={this.handleEmailChange}
-								/>
+						<rendition.Divider color="#eee" mb={4}/>
 
-								<rendition.Txt fontSize={1} mb={1}>Username</rendition.Txt>
-								<rendition.Input
-									className="login-page__input--username"
-									mb={5}
+						<form onSubmit={this.login}>
+							<rendition.Txt fontSize={1} mb={1}>Username</rendition.Txt>
+							<rendition.Input
+								className="login-page__input--username"
+								mb={5}
+								width="100%"
+								emphasized={true}
+								placeholder="Username"
+								value={username}
+								onChange={this.handleUsernameChange}
+							/>
+
+							<rendition.Txt fontSize={1} mb={1}>Password</rendition.Txt>
+							<rendition.Input
+								className="login-page__input--password"
+								mb={5}
+								width="100%"
+								emphasized={true}
+								placeholder="Password"
+								type="password"
+								value={password}
+								onChange={this.handlePasswordChange}
+							/>
+
+							<rendition.Box>
+								<rendition.Button
+									className="login-page__submit--login"
 									width="100%"
+									primary={true}
 									emphasized={true}
-									placeholder="Username"
-									value={this.state.username}
-									onChange={this.handleUsernameChange}
-								/>
-
-								<rendition.Flex mb={5}>
-									<rendition.Box mr={3} flex="1">
-										<rendition.Txt fontSize={1} mb={1}>Password</rendition.Txt>
-										<rendition.Input
-											className="login-page__input--password"
-											width="100%"
-											emphasized={true}
-											placeholder="Password"
-											type={this.state.showPassword ? 'text' : 'password'}
-											value={this.state.password}
-											onChange={this.handlePasswordChange}
-										/>
-									</rendition.Box>
-
-									<rendition.Box mr={2} flex="1">
-										<rendition.Txt fontSize={1} mb={1}>Confirm password</rendition.Txt>
-										<rendition.Input
-											className="login-page__input--confirm-password"
-											w="100%"
-											emphasized={true}
-											placeholder="Confirm
-											password"
-											type={this.state.showPassword ? 'test' : 'password'}
-											value={this.state.passwordConfirmation}
-											onChange={this.handlePasswordConfirmationChange}
-										/>
-									</rendition.Box>
-
-									<rendition.Button
-										square
-										plain
-										mt={20}
-										type="button"
-										onClick={this.togglePasswordVisibility}
-									>
-										<Icon.default name={this.state.showPassword ? 'eye' : 'eye-slash'}/>
-									</rendition.Button>
-								</rendition.Flex>
-
-								<rendition.Box>
-									<rendition.Button
-										className="login-page__submit--signup"
-										width="100%"
-										primary={true}
-										emphasized={true}
-										disabled={signupDisabled}
-										onClick={this.signup}
-									>
-										{this.state.loggingIn ? <Icon.default spin name="cog"/> : 'Sign up'}
-									</rendition.Button>
-								</rendition.Box>
-								<rendition.Txt color="#908c99" fontSize={0} my={4} align="center">
-									By clicking &#34;Sign up&#34; you confirm that you have performed the
-									<rendition.Link
-										ml={1}
-										blank={true}
-										href="https://bosshamster.deviantart.com/art/Summoning-Cthulhu-For-Dummies-31645860"
-									>
-										initiation rituals
-									</rendition.Link>
-								</rendition.Txt>
-							</form>
-						</React.Fragment>
-					)}
-
-					{!this.state.showSignup && (
-						<React.Fragment>
-							<rendition.Txt align="center" mb={4}>
-								<rendition.Heading.h2 mb={2}>Log in to Jellyfish</rendition.Heading.h2>
-								<span>Enter your details below</span>
-							</rendition.Txt>
-
-							<rendition.Divider color="#eee" mb={4}/>
-
-							<form onSubmit={this.login}>
-								<rendition.Txt fontSize={1} mb={1}>Username</rendition.Txt>
-								<rendition.Input
-									className="login-page__input--username"
-									mb={5}
-									width="100%"
-									emphasized={true}
-									placeholder="Username"
-									value={this.state.username}
-									onChange={this.handleUsernameChange}
-								/>
-
-								<rendition.Txt fontSize={1} mb={1}>Password</rendition.Txt>
-								<rendition.Input
-									className="login-page__input--password"
-									mb={5}
-									width="100%"
-									emphasized={true}
-									placeholder="Password"
-									type="password"
-									value={this.state.password}
-									onChange={this.handlePasswordChange}
-								/>
-
-								<rendition.Box>
-									<rendition.Button
-										className="login-page__submit--login"
-										width="100%"
-										primary={true}
-										emphasized={true}
-										disabled={!this.state.username || !this.state.password || this.state.loggingIn}
-										onClick={this.login}
-									>
-										{this.state.loggingIn ? <Icon.default spin name="cog"/> : 'Log in'}
-									</rendition.Button>
-								</rendition.Box>
-							</form>
-						</React.Fragment>
-					)}
-				</rendition.Box>
-			</rendition.Container>
-		</React.Fragment>)
+									type="submit"
+									disabled={!username || !password || loggingIn}
+								>
+									{loggingIn ? <Icon.default spin name="cog"/> : 'Log in'}
+								</rendition.Button>
+							</rendition.Box>
+						</form>
+					</rendition.Box>
+				</rendition.Container>
+			</React.Fragment>
+		)
 	}
 }
 const mapDispatchToProps = (dispatch) => {
 	return {
 		actions: {
 			addNotification: redux.bindActionCreators(actionCreators.addNotification, dispatch),
-			login: redux.bindActionCreators(actionCreators.login, dispatch),
-			signup: redux.bindActionCreators(actionCreators.signup, dispatch)
+			login: redux.bindActionCreators(actionCreators.login, dispatch)
 		}
 	}
 }
