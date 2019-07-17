2a52204a45f9f3ac80c12bc29a077c1685e2c57d
Juan Cruz Viotti <juan@balena.io>
2019-07-17 19:59:47 +0100

    kubefish: Better container autocompletion and new explore commands
    Change-type: patch
    Signed-off-by: Juan Cruz Viotti <juan@balena.io>

--- scripts/kubefi.sh
+++ scripts/kubefi.sh
@@ -22,7 +22,7 @@ check() {
 
 get_container() (
 	pod_names="$(kubectl get pods -n jellyfish -o custom-columns=:metadata.name)"
-	echo "$pod_names" | grep "^$1" || true
+	echo "$pod_names" | grep "^$1" | head -n 1 || true
 )
 
 usage() {
@@ -43,12 +43,15 @@ usage() {
 	echo "    restart <container> Restart a container" 1>&2
 	echo "    logs <container>    Stream the logs from a container" 1>&2
 	echo "    shell <container>   Open a shell on a container" 1>&2
+	echo "    slug <slug>         Pretty print card by slug" 1>&2
+	echo "    id <id>             Pretty print card by id" 1>&2
 	echo "" 1>&2
 	echo "Examples:" 1>&2
 	echo "" 1>&2
 	echo "    $0 status" 1>&2
-	echo "    $0 logs jellyfish" 1>&2
-	echo "    $0 shell jellyfish" 1>&2
+	echo "    $0 logs jellyfish-ui-5bd87fbb4-29zj5" 1>&2
+	echo "    $0 shell action-server" 1>&2
+	echo "    $0 slug user-admin" 1>&2
 	exit 1
 }
 
@@ -67,6 +70,7 @@ trap 'kill $(jobs -pr)' INT TERM EXIT
 
 # Open the SSH tunnel in the background, and wait
 # for a while to make sure its available
+echo "Starting SSH tunnel..." 1>&2
 ssh -N -L 8443:api.uswest2.k8s.resinstaging.io:443 admin@bastion.uswest2.k8s.resinstaging.io &
 sleep 3
 
@@ -79,7 +83,7 @@ if [ "$COMMAND" = "restart" ]; then
 	check "$SUBCOMMAND" "You must specify a container as an argument"
 	CONTAINER="$(get_container "$SUBCOMMAND")"
 	check "$CONTAINER" "Invalid container: $SUBCOMMAND"
-	echo "Restarting $CONTAINER..."
+	echo "Restarting $CONTAINER..." 1>&2
 	kubectl delete pod "$CONTAINER" --namespace "$K8S_NAMESPACE"
 	exit 0
 fi
@@ -88,7 +92,7 @@ if [ "$COMMAND" = "logs" ]; then
 	check "$SUBCOMMAND" "You must specify a container as an argument"
 	CONTAINER="$(get_container "$SUBCOMMAND")"
 	check "$CONTAINER" "Invalid container: $SUBCOMMAND"
-	echo "Getting logs from $CONTAINER..."
+	echo "Getting logs from $CONTAINER..." 1>&2
 	kubectl logs --follow "$CONTAINER" --namespace "$K8S_NAMESPACE"
 	exit 0
 fi
@@ -97,8 +101,30 @@ if [ "$COMMAND" = "shell" ]; then
 	check "$SUBCOMMAND" "You must specify a container as an argument"
 	CONTAINER="$(get_container "$SUBCOMMAND")"
 	check "$CONTAINER" "Invalid container: $SUBCOMMAND"
-	echo "Starting shell at $CONTAINER..."
+	echo "Starting shell at $CONTAINER..." 1>&2
 	kubectl exec --stdin --tty "$CONTAINER" bash --namespace "$K8S_NAMESPACE"
+	exit 0
+fi
+
+if [ "$COMMAND" = "slug" ]; then
+	check "$SUBCOMMAND" "You must specify a slug as an argument"
+	CONTAINER="$(get_container "action-server")"
+	check "$CONTAINER" "No workers alive"
+	echo "Running command at $CONTAINER..." 1>&2
+	kubectl exec "$CONTAINER" \
+		node scripts/production/explorer.js slug "$SUBCOMMAND" \
+		--namespace "$K8S_NAMESPACE"
+	exit 0
+fi
+
+if [ "$COMMAND" = "id" ]; then
+	check "$SUBCOMMAND" "You must specify an id as an argument"
+	CONTAINER="$(get_container "action-server")"
+	check "$CONTAINER" "No workers alive"
+	echo "Running command at $CONTAINER..." 1>&2
+	kubectl exec "$CONTAINER" \
+		node scripts/production/explorer.js id "$SUBCOMMAND" \
+		--namespace "$K8S_NAMESPACE"
 	exit 0
 fi
 
