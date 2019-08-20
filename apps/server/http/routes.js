2f8b1a242690b66ffcfc1c4d740d21b31333b4b6
Juan Cruz Viotti <juan@balena.io>
2019-08-20 21:30:04 +0100

    server: Log webhook error request body
    Change-type: patch
    Signed-off-by: Juan Cruz Viotti <juan@balena.io>

--- apps/server/http/routes.js
+++ apps/server/http/routes.js
@@ -384,6 +384,7 @@ module.exports = (application, jellyfish, worker, queue) => {
 				})
 			})
 		}).catch((error) => {
+			error.body = request.body
 			logger.exception(request.context, 'Webhook error', error)
 			return response.status(500).json({
 				error: true,
