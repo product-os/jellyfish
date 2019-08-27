47dd12393f0f67f5ceac18676da2a20504e87455
Kakhaber <kaxabernet@gmail.com>
2019-08-27 17:54:54 +0400

    Add short thread list
    Change-type: minor

---
+++ lib/chat-widget/components/Layout.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+	Flex
+} from 'rendition'
+import {
+	Task
+} from './Task'
+import {
+	useActions,
+	useCombineTasks,
+	useTask
+} from '../hooks'
+
+export const Layout = ({
+	children, ...rest
+}) => {
+	const actions = useActions()
+	const fetchThreads = useTask(actions.fetchThreads)
+	const setCurrentUser = useTask(actions.setCurrentUser)
+	const combinedTask = useCombineTasks(fetchThreads, setCurrentUser)
+
+	React.useEffect(() => {
+		fetchThreads.exec({
+			limit: 15
+		})
+
+		setCurrentUser.exec()
+	}, [])
+
+	return (
+		<Flex {...rest} flexDirection="column">
+			Header
+			<Flex flex={1} flexDirection="column">
+				<Task task={combinedTask}>{() => { return children }}</Task>
+			</Flex>
+		</Flex>
+	)
+}
---
+++ lib/chat-widget/components/Task.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {
+	Txt
+} from 'rendition'
+import Icon from '../../ui/shame/Icon'
+import {
+	ErrorMessage
+} from './ErrorMessage'
+
+export const Task = ({
+	task,
+	loader = <Icon spin name="cog"/>,
+	children
+}) => {
+	if (!task.finished) {
+		return loader
+	}
+
+	if (task.error) {
+		return (
+			<React.Fragment>
+				<ErrorMessage error={task.error} />{' '}<Txt.span onClick={task.retry}>Retry</Txt.span>
+			</React.Fragment>
+		)
+	}
+
+	return children(task.result)
+}
