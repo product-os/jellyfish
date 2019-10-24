471c325d718440abf62e62999d24b44d8a752a58
Scott Lowe <scott@balena.io>
2019-10-24 13:47:12 +0200

    action: Add google meet action
    Change-type: minor
    Signed-off-by: Scott Lowe <scott@balena.io>

---
+++ test/e2e/server/actions/action-google-meet.spec.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+const ava = require('ava')
+const helpers = require('../../sdk/helpers')
+const environment = require('../../../../lib/environment')
+
+ava.before(helpers.before)
+ava.after(helpers.after)
+
+ava.beforeEach(helpers.beforeEach)
+ava.afterEach(helpers.afterEach)
+
+if (environment.integration['google-meet'].credentials) {
+	ava.serial('should create a new google meet using action-google-meet', async (test) => {
+		const admin = await test.context.sdk.card.get('user-admin')
+
+		const session = await test.context.sdk.card.create({
+			type: 'session',
+			slug: test.context.generateRandomSlug({
+				prefix: 'session'
+			}),
+			version: '1.0.0',
+			data: {
+				actor: admin.id
+			}
+		})
+
+		const result = await test.context.http(
+			'POST', '/api/v2/action', {
+				card: 'card',
+				type: 'type',
+				action: 'action-google-meet',
+				arguments: {
+					reason: null,
+					properties: {
+						slug: test.context.generateRandomSlug({
+							prefix: 'google-meet-test'
+						})
+					}
+				}
+			}, {
+				Authorization: `Bearer ${session.id}`
+			})
+
+		const id = result.response.data.id
+
+		const updatedCard = await test.context.sdk.card.get(id)
+
+		const googleMeetRegex = /https:\/\/meet\.google\.com\/[a-z]{3}-[a-z]{4}-[a-z]{3}/
+
+		test.truthy(updatedCard && updatedCard.data.conferenceUrl.match(googleMeetRegex))
+	})
+}
