19138ddb473940d632e8a1256ae646f1c8272703
Giovanni Garufi <giovanni@balena.io>
2019-10-24 13:04:44 +0200

    Update deploy-templates for katapult v2
    Change-type: patch
    Signed-off-by: Giovanni Garufi <giovanni@balena.io>

--- deploy-templates/deploy.sh
+++ deploy-templates/deploy.sh
@@ -11,24 +11,8 @@ if [ "$#" -ne 1 ] ;then
 	exit 1
 fi
 
-# patch /etc/hosts for tunneling k8s api
-echo "127.0.0.1 $K8S_STG_API" >> /etc/hosts
-echo "hosts: files dns" > /etc/nsswitch.conf
-
 # Install kubectl
 wget -O kubectl "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" && chmod +x ./kubectl && mv kubectl /usr/local/bin/
 
-# Generate environments.yml
-cp ./environments.tpl.yml ./environments.yml
-sed -i "s/K8S_STG_BASTION_USERNAME/$K8S_STG_BASTION_USERNAME/g" ./environments.yml
-sed -i "s/K8S_STG_BASTION/$K8S_STG_BASTION/g" ./environments.yml
-sed -i "s/K8S_STG_API/$K8S_STG_API/g" ./environments.yml
-
-# Get environments.yml required files (kubeconfig, jellyfish_key)
-echo "$JELLYFISH_STG_KUBECONFIG" | base64 -d > ./kubeconfig
-echo "$K8S_STG_BASTION_KEY" | base64 -d > jellyfish_pk
-chmod 400 jellyfish_pk
-
 # Deploy with katapult
-katapult deploy -t kubernetes -e staging -c . -v -k "$1"
-
+KATAPULT_KUBE_CONFIG="$(echo "$KATAPULT_KUBE_CONFIG" | base64 -d)" katapult deploy -t kubernetes -e jellfish-product
