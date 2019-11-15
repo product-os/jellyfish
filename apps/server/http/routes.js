9655f70a1cdbc66ba15b9ee4ed9af8d5d8a78553
Lucian <lucian.buzzo@gmail.com>
2019-11-15 17:09:29 +0000

    Move SDK whoami() method to the server
    Change-type: minor
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- apps/server/http/routes.js
+++ apps/server/http/routes.js
@@ -523,6 +523,49 @@ module.exports = (application, jellyfish, worker, queue) => {
 		})
 	})
 
+	const queryAPI = (context, sessionToken, query, options, request) => {
+		return Bluebird.try(async () => {
+			if (!_.isString(query)) {
+				return query
+			}
+
+			// Now try and load the view by slug
+			const viewCardFromSlug = await jellyfish.getCardBySlug(
+				context, sessionToken, `${query}@latest`)
+
+			if (viewCardFromSlug && viewCardFromSlug.type === 'view') {
+				return viewCardFromSlug
+			}
+
+			try {
+				// Try and load the view by id first
+				const viewCardFromId = await jellyfish.getCardById(context, sessionToken, query)
+
+				if (!viewCardFromId || viewCardFromId.type !== 'view') {
+					throw new jellyfish.errors.JellyfishNoView(
+						`Unknown view: ${query}`)
+				}
+
+				return viewCardFromId
+			} catch (error) {
+				throw new jellyfish.errors.JellyfishNoView(
+					`Unknown view: ${query}`)
+			}
+		}).then(async (schema) => {
+			const startDate = new Date()
+			const data = await jellyfish.query(context, sessionToken, schema, options)
+			const endDate = new Date()
+			const queryTime = endDate.getTime() - startDate.getTime()
+			logger.info(context, 'JSON Schema query', {
+				time: queryTime,
+				ip: request.ip,
+				schema
+			})
+
+			return data
+		})
+	}
+
 	application.post('/api/v2/query', (request, response) => {
 		if (_.isEmpty(request.body)) {
 			return response.status(400).json({
@@ -531,54 +574,113 @@ module.exports = (application, jellyfish, worker, queue) => {
 			})
 		}
 
-		return Bluebird.try(async () => {
-			if (!_.isString(request.body.query)) {
-				return request.body.query
-			}
-
-			// Now try and load the view by slug
-			const viewCardFromSlug = await jellyfish.getCardBySlug(
-				request.context, request.sessionToken, `${request.body.query}@latest`)
-
-			if (viewCardFromSlug && viewCardFromSlug.type === 'view') {
-				return viewCardFromSlug
-			}
-
-			try {
-				// Try and load the view by id first
-				const viewCardFromId = await jellyfish.getCardById(
-					request.context, request.sessionToken, request.body.query)
-
-				if (!viewCardFromId || viewCardFromId.type !== 'view') {
-					throw new jellyfish.errors.JellyfishNoView(
-						`Unknown view: ${request.body.query}`)
+		return queryAPI(
+			request.context,
+			request.sessionToken,
+			request.body.query,
+			request.body.options,
+			request
+		)
+			.then((data) => {
+				return response.status(200).json({
+					error: false,
+					data
+				})
+			}).catch((error) => {
+				if (error.expected) {
+					response.status(400).json({
+						error: true,
+						data: {
+							name: error.name,
+							message: error.message
+						}
+					})
+					return
 				}
 
-				return viewCardFromId
-			} catch (error) {
-				throw new jellyfish.errors.JellyfishNoView(
-					`Unknown view: ${request.body.query}`)
-			}
-		}).then(async (schema) => {
-			request.payload = schema
-			const startDate = new Date()
-			const data = await jellyfish.query(
-				request.context, request.sessionToken, schema, request.body.options)
-			const endDate = new Date()
-			const queryTime = endDate.getTime() - startDate.getTime()
-			logger.info(request.context, 'JSON Schema query', {
-				time: queryTime,
-				ip: request.ip,
-				schema
+				response.status(500).json({
+					error: true,
+					data: error.message
+				})
 			})
+	})
+
+	application.get('/api/v2/whoami', async (request, response) => {
+		try {
+			const result = await jellyfish.getCardById(
+				request.context, request.sessionToken, request.sessionToken)
+
+			if (!result) {
+				throw new Error('Could not retrieve session data')
+			}
+
+			const schema = {
+				type: 'object',
+				$$links: {
+					'is member of': {
+						type: 'object',
+						additionalProperties: true
+					}
+				},
+				properties: {
+					id: {
+						type: 'string',
+						const: result.data.actor
+					},
+					type: {
+						type: 'string',
+						const: 'user'
+					},
+					links: {
+						type: 'object',
+						additionalProperties: true
+					}
+				},
+				required: [ 'id' ],
+				additionalProperties: true
+			}
+
+			// Try and load the user with attached org data, otherwise load them
+			// without it.
+			// TODO: Fix our broken queries so that we can optionally get linked
+			// data
+			let user = await queryAPI(
+				request.context,
+				request.sessionToken,
+				schema,
+				{
+					limit: 1
+				},
+				request
+			)
+				.then((elements) => {
+					return elements[0] || null
+				})
+
+			if (!user) {
+				user = await jellyfish.getCardById(
+					request.context, request.sessionToken, result.data.actor)
+			}
 
 			return response.status(200).json({
 				error: false,
-				data
+				data: user
 			})
-		}).catch((error) => {
-			logger.warn(request.context, 'JSON Schema query error', request.body)
-			return sendHTTPError(request, response, error)
-		})
+		} catch (error) {
+			if (error.expected) {
+				return response.status(400).json({
+					error: true,
+					data: {
+						name: error.name,
+						message: error.message
+					}
+				})
+			}
+
+			return response.status(500).json({
+				error: true,
+				data: error.message
+			})
+		}
 	})
 }
