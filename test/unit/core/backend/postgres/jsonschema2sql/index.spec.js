2d0595c1a697b15cdffe55bbe2cdfa421dd5e4d3
Federico Fissore <federico@fissore.org>
2019-11-19 17:42:48 +0100

    Using the @> operator on array columns as well
    Connects-to: #2780
    Change-type: patch
    Signed-off-by: Federico Fissore <federico@fissore.org>

--- test/unit/core/backend/postgres/jsonschema2sql/index.spec.js
+++ test/unit/core/backend/postgres/jsonschema2sql/index.spec.js
@@ -7,7 +7,7 @@
 const ava = require('ava')
 const jsonschema2sql = require('../../../../../../lib/core/backend/postgres/jsonschema2sql')
 
-ava('schema with type array and string const is translated to a query that uses the @> operator', (test) => {
+ava('when querying for jsonb array field that contains string const we use the @> operator', (test) => {
 	const query = jsonschema2sql('cards', {
 		type: 'object',
 		required: [ 'type', 'data' ],
@@ -48,7 +48,7 @@ OR
 (cards.data->'mirrors' @> '"https://api2.frontapp.com/conversations/cnv_2q9efia"')))`)
 })
 
-ava('schema with type array and number const is translated to a query that uses the @> operator', (test) => {
+ava('when querying for jsonb array field that contains number const we use the @> operator', (test) => {
 	const query = jsonschema2sql('cards', {
 		type: 'object',
 		required: [ 'type', 'data' ],
@@ -87,4 +87,48 @@ AND
 ((cards.data->'mirrors' IS NULL)
 OR
 (cards.data->'mirrors' @> '42')))`)
+})
+
+ava('when querying for array that contains string const we use the @> operator', (test) => {
+	const query = jsonschema2sql('cards', {
+		type: 'object',
+		anyOf: [ {
+			type: 'object'
+		} ],
+		required: [ 'data', 'tags' ],
+		properties: {
+			data: {
+				type: 'object',
+				required: [ 'number' ],
+				properties: {
+					number: {
+						type: 'number', const: 1
+					}
+				}
+			},
+			tags: {
+				type: 'array',
+				contains: {
+					type: 'string',
+					const: 'foo'
+				}
+			}
+		},
+		additionalProperties: false
+	})
+
+	test.deepEqual(query, `SELECT
+cards."data",
+cards."tags"
+FROM cards
+WHERE
+((cards.data->'number' IS NOT NULL)
+AND
+((cards.data->'number' IS NULL)
+OR
+((jsonb_typeof(cards.data->'number') = 'number')
+AND
+(cards.data->'number' @> '1'))))
+AND
+(cards.tags @> ARRAY['foo'])`)
 })
