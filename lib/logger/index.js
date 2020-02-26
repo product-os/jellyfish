product-os/jellyfish

commit e03c806f5b2204f1c27b43899b34953f53e10845

Improve readability of logger, exit immediately when log level doesn't match
Change-type: patch
Signed-off-by: Federico Fissore <federico@balena.io>

lib/logger/index.js
@@ -47,9 +47,6 @@ class Logger {
 	}
 
 	debug (context, message, data) {
-		// TODO: Investigate the LogEntries transport,
-		// as it seems like its not obeying the Winston
-		// instance level.
 		if (this.level !== 'debug') {
 			return
 		}
@@ -63,6 +60,10 @@ class Logger {
 	}
 
 	error (context, message, data) {
+		if (this.level !== 'crit') {
+			return
+		}
+
 		this.logger.log('crit', message, {
 			namespace: this.namespace,
 			version: packageJSON.version,
@@ -72,6 +73,10 @@ class Logger {
 	}
 
 	warn (context, message, data) {
+		if (this.level !== 'warning') {
+			return
+		}
+
 		this.logger.log('warning', message, {
 			namespace: this.namespace,
 			version: packageJSON.version,
@@ -81,6 +86,10 @@ class Logger {
 	}
 
 	info (context, message, data) {
+		if (this.level !== 'info') {
+			return
+		}
+
 		this.logger.log('info', message, {
 			namespace: this.namespace,
 			version: packageJSON.version,
@@ -104,28 +113,34 @@ class Logger {
 	}
 }
 
-module.exports.getLogger = _.memoize((filename) => {
-	const transport = environment.isProduction() && environment.logentries.token
-		? new winston.transports.Logentries({
+const newTransport = () => {
+	if (environment.isProduction() && environment.logentries.token) {
+		return new winston.transports.Logentries({
 			token: environment.logentries.token
 		})
-		: new winston.transports.Console({
-			format: winston.format.combine(
-				winston.format.colorize(),
-				winston.format.printf((info) => {
-					assert.INTERNAL(null, info.context && info.context.id,
-						LoggerNoContext, `Missing context: ${JSON.stringify(info)}`)
+	}
 
-					const id = info.context.id
-					const prefix = `(${info.version}) ${info.timestamp} [${info.namespace}] [${info.level}] [${id}]:`
-					if (info.data) {
-						return `${prefix} ${info.message} ${JSON.stringify(info.data)}`
-					}
+	return new winston.transports.Console({
+		format: winston.format.combine(
+			winston.format.colorize(),
+			winston.format.printf((info) => {
+				assert.INTERNAL(null, info.context && info.context.id,
+					LoggerNoContext, `Missing context: ${JSON.stringify(info)}`)
 
-					return `${prefix} ${info.message}`
-				}))
-		})
+				const id = info.context.id
+				const prefix = `(${info.version}) ${info.timestamp} [${info.namespace}] [${info.level}] [${id}]:`
+				if (info.data) {
+					return `${prefix} ${info.message} ${JSON.stringify(info.data)}`
+				}
 
-	const logLevel = environment.logger.loglevel || 'debug'
+				return `${prefix} ${info.message}`
+			}))
+	})
+}
+
+const transport = newTransport()
+const logLevel = environment.logger.loglevel || 'debug'
+
+module.exports.getLogger = _.memoize((filename) => {
 	return new Logger(logLevel, filename, transport)
 })

commit 616cab2c9b67907e5e9b0a46998bba00d4a3666e

Fix bold mistake introduced in e03c806f5b2204f1c27b43899b34953f53e10845
We are not currently logging unless levels matche, but that's NOT how
log levels work: if log level is 'debug' and we log an exception, it
must be logged

Change-type: patch
Signed-off-by: Federico Fissore <federico@balena.io>

lib/logger/index.js
@@ -19,6 +19,7 @@ const BASE_PATH = path.join(__dirname, '..', '..')
 require('le_node')
 
 const LoggerNoContext = typedErrors.makeTypedError('LoggerNoContext')
+const LEVELS = winston.config.syslog.levels
 
 class Logger {
 	constructor (level, filename, transport) {
@@ -47,7 +48,7 @@ class Logger {
 	}
 
 	debug (context, message, data) {
-		if (this.level !== 'debug') {
+		if (LEVELS[this.level] < LEVELS.debug) {
 			return
 		}
 
@@ -60,7 +61,7 @@ class Logger {
 	}
 
 	error (context, message, data) {
-		if (this.level !== 'crit') {
+		if (LEVELS[this.level] < LEVELS.crit) {
 			return
 		}
 
@@ -73,7 +74,7 @@ class Logger {
 	}
 
 	warn (context, message, data) {
-		if (this.level !== 'warning') {
+		if (LEVELS[this.level] < LEVELS.warning) {
 			return
 		}
 
@@ -86,7 +87,7 @@ class Logger {
 	}
 
 	info (context, message, data) {
-		if (this.level !== 'info') {
+		if (LEVELS[this.level] < LEVELS.info) {
 			return
 		}
 