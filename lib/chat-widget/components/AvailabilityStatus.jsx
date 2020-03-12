06e3e21c3da2bf044badbf136996670e0a9cf389
Kakhaber <kaxabernet@gmail.com>
2020-03-12 15:43:35 +0400

    Hide availability status when available
    Change-type: minor

--- lib/chat-widget/components/AvailabilityStatus.jsx
+++ lib/chat-widget/components/AvailabilityStatus.jsx
@@ -5,39 +5,24 @@
  */
 
 import React from 'react'
-import CircleIcon from 'react-icons/lib/fa/circle'
 import {
-	Box, Txt, useTheme
+	Txt
 } from 'rendition'
 import {
 	useIsInUTCHourRange
 } from '../hooks'
 
-export const AvailabilityStatusOnline = () => {
-	const theme = useTheme()
+export const AvailabilityStatus = () => {
+	const isInRange = useIsInUTCHourRange(8, 24)
 
-	return (
-		<Box>
-			<CircleIcon size="8px" color={theme.colors.success.main} />{' '}Online
-		</Box>
-	)
-}
+	if (isInRange) {
+		return null
+	}
 
-export const AvailabilityStatusOffline = () => {
 	return (
 		<Txt fontSize="12px" align="center">
             Available hours: <br />
 			<b>8 am – 12 pm (GMT)</b>
 		</Txt>
-	)
-}
-
-export const AvailabilityStatus = () => {
-	const isInRange = useIsInUTCHourRange(8, 24)
-
-	return isInRange ? (
-		<AvailabilityStatusOnline />
-	) : (
-		<AvailabilityStatusOffline />
 	)
 }
