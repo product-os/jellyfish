8fc1fdb0a9a1099613f27608537f086a6d7d4cb0
Graham McCulloch <admin@grahammcculloch.co.uk>
2020-03-12 09:39:27 +0700

    ui-components: Add prev/next/action buttons to StepsFlow component
    Added unit tests to verify behaviour.
    
    Change-type: minor
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- lib/ui-components/StepsFlow.spec.jsx
+++ lib/ui-components/StepsFlow.spec.jsx
@@ -28,51 +28,106 @@ const stepsProps = {
 	titleText: 'Test flow'
 }
 
+const getStep = (index, stepStatus) => {
+	return (
+		<StepsFlow.Step key={index} label={`Step ${index}`} status={stepStatus}>
+			<div className={`step${index}`}>Step {index}</div>
+		</StepsFlow.Step>
+	)
+}
+
+const getShallowStepsFlow = ({
+	status, ...props
+}) => {
+	const stepStatus = status || 'pending'
+	return shallow(
+		<StepsFlow stepsProps={stepsProps} {...props}>
+			{[ 1, 2, 3 ].map((index) => { return getStep(index, stepStatus) })}
+		</StepsFlow>
+	)
+}
+
+const getMountedStepsFlow = ({
+	status, ...props
+}) => {
+	const stepStatus = status || 'pending'
+	return mount(
+		<Provider>
+			<StepsFlow stepsProps={stepsProps} {...props}>
+				{[ 1, 2, 3 ].map((index) => { return getStep(index, stepStatus) })}
+			</StepsFlow>
+		</Provider>
+	)
+}
+
 ava('StepsFlow should render', (test) => {
 	test.notThrows(() => {
-		shallow(
-			<StepsFlow stepsProps={stepsProps}>
-				<StepsFlow.Step label="Step 1" status="completed">
-					<div className="step1">Step 1</div>
-				</StepsFlow.Step>
-				<StepsFlow.Step label="Step 2" status="pending">
-					<div className="step2">Step 2</div>
-				</StepsFlow.Step>
-			</StepsFlow>
-		)
+		getShallowStepsFlow({})
 	})
 })
 
 ava('initialStep is respected', (test) => {
-	const component = mount(
-		<Provider>
-			<StepsFlow initialStep={1} stepsProps={stepsProps}>
-				<StepsFlow.Step label="Step 1" status="completed">
-					<div className="step1">Step 1</div>
-				</StepsFlow.Step>
-				<StepsFlow.Step label="Step 2" status="pending">
-					<div className="step2">Step 2</div>
-				</StepsFlow.Step>
-			</StepsFlow>
-		</Provider>
-	)
+	const component = getMountedStepsFlow({
+		initialStep: 1
+	})
 
 	const stepHeading = component.find('h5[data-test="flow-step-1__heading"]')
 	test.is(stepHeading.text(), 'Step 2')
 })
 
+ava('Previous button is disabled on first step', (test) => {
+	const component = getMountedStepsFlow({
+		status: 'completed'
+	})
+
+	let prevButton = component.find('button[data-test="steps-flow__prev-btn"]')
+	test.is(prevButton.prop('disabled'), true)
+
+	const nextButton = component.find('button[data-test="steps-flow__next-btn"]')
+	test.is(nextButton.prop('disabled'), false)
+
+	nextButton.simulate('click')
+
+	prevButton = component.find('button[data-test="steps-flow__prev-btn"]')
+	test.is(prevButton.prop('disabled'), false)
+})
+
+ava('Next button is disabled if step is not completed', (test) => {
+	const component = getMountedStepsFlow({
+		status: 'pending'
+	})
+
+	const nextButton = component.find('button[data-test="steps-flow__next-btn"]')
+	test.is(nextButton.prop('disabled'), true)
+})
+
+ava('Action button is displayed on last step', (test) => {
+	const component = getMountedStepsFlow({
+		initialStep: 2
+	})
+
+	const actionButton = component.find('button[data-test="steps-flow__action-btn"]')
+	test.is(actionButton.prop('disabled'), true)
+
+	const nextButton = component.find('button[data-test="steps-flow__next-btn"]')
+	test.false(nextButton.exists())
+})
+
+ava('Action button is enabled if all steps are completed', (test) => {
+	const component = getMountedStepsFlow({
+		initialStep: 2,
+		status: 'completed'
+	})
+
+	const actionButton = component.find('button[data-test="steps-flow__action-btn"]')
+	test.is(actionButton.prop('disabled'), false)
+})
+
 ava('initialStep cannot be greater than number of steps', (test) => {
 	test.throws(() => {
-		shallow(
-			<StepsFlow initialStep={3} stepsProps={stepsProps}>
-				<StepsFlow.Step label="Step 1" status="completed">
-					<div className="step1">Step 1</div>
-				</StepsFlow.Step>
-				<StepsFlow.Step label="Step 2" status="pending">
-					<div className="step2">Step 2</div>
-				</StepsFlow.Step>
-			</StepsFlow>
-		)
+		getShallowStepsFlow({
+			initialStep: 3
+		})
 	})
 })
 
@@ -91,12 +146,8 @@ ava('Invalid child components are not allowed', (test) => {
 
 ava('Invalid status values are not allowed', (test) => {
 	test.throws(() => {
-		shallow(
-			<StepsFlow initialStep={3} stepsProps={stepsProps}>
-				<StepsFlow.Step label="Step 1" status="unknown">
-					<div className="step1">Step 1</div>
-				</StepsFlow.Step>
-			</StepsFlow>
-		)
+		getShallowStepsFlow({
+			status: 'foobar'
+		})
 	})
 })
