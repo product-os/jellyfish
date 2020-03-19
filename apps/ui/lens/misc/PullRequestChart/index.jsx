9748e9cb24c1a99e787819956ed59f68a46e35d0
Lucian Buzzo <lucian.buzzo@gmail.com>
2020-03-19 11:08:13 +0000

    Add a basic chart lens for pull requests
    Change-type: minor
    Connects-to: #3303
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

---
+++ apps/ui/lens/misc/PullRequestChart/index.jsx
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+import * as _ from 'lodash'
+import {
+	connect
+} from 'react-redux'
+import * as redux from 'redux'
+import {
+	actionCreators,
+	selectors
+} from '../../../core'
+import PullRequestChart from './PullRequestChart'
+
+const mapStateToProps = (state, ownProps) => {
+	return {
+		channels: selectors.getChannels(state)
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		actions: redux.bindActionCreators(
+			_.pick(actionCreators, [
+				'getActor'
+			]),
+			dispatch
+		)
+	}
+}
+
+const lens = {
+	slug: 'lens-pull-request-chart',
+	type: 'lens',
+	version: '1.0.0',
+	name: 'Pull request chart lens',
+	data: {
+		icon: 'chart-bar',
+		format: 'list',
+		renderer: connect(mapStateToProps, mapDispatchToProps)(PullRequestChart),
+		filter: {
+			type: 'array',
+			items: {
+				type: 'object',
+				properties: {
+					id: {
+						type: 'string'
+					}
+				}
+			}
+		},
+		queryOptions: {
+			mask: (query) => {
+				Reflect.deleteProperty(query, '$$links')
+				query.properties.data = {
+					type: 'object',
+					additionalProperties: true
+				}
+
+				return query
+			},
+			limit: 1000,
+			sortBy: [ 'created_at' ],
+			sortDir: 'desc'
+		}
+	}
+}
+
+export default lens
