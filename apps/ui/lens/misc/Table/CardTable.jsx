df7894e07cc83985725d1d7ef61a3266f31b9a24
StefKors <stef.kors@gmail.com>
2020-03-30 19:27:58 +0200

    Generate CardTable table columns from card schema
    Fixes #3440
    
    Change-type: minor
    Signed-off-by: Stef Kors <stef@balena.io>

--- apps/ui/lens/misc/Table/CardTable.jsx
+++ apps/ui/lens/misc/Table/CardTable.jsx
@@ -13,48 +13,74 @@ import {
 	Table,
 	Txt
 } from 'rendition'
+import flatten from 'flat'
 import BaseLens from '../../common/BaseLens'
 import Link from '../../../../../lib/ui-components/Link'
 import Column from '../../../../../lib/ui-components/shame/Column'
+import {
+	getTypeFields
+} from './helpers'
 
 const PAGE_SIZE = 25
-const COLUMNS = [
-	{
-		field: 'name',
-		sortable: true,
-		render: (value, item) => {
-			return <Link append={item.slug || item.id}>{value}</Link>
-		}
-	},
-	{
-		field: 'Created',
-		sortable: true
-	},
-	{
-		field: 'Last updated',
-		sortable: true
+
+// For some columns like card.name we use a render function
+const RENDERERS = {
+	name: (name, item) => {
+		return <Link append={item.slug || item.id}>{name}</Link>
 	}
-]
+}
 
 export default class CardTable extends BaseLens {
 	constructor (props) {
 		super(props)
 		this.generateTableData = this.generateTableData.bind(this)
+		this.generateTableColumns = this.generateTableColumns.bind(this)
+	}
+
+	generateTableColumns () {
+		// GenerateTableColumns generates the default table columns
+		// First, we get all the typeFields we want.
+		const typeFields = getTypeFields(this.props.type)
+
+		// Second, we create and return the tableColumns array
+		return _.map(typeFields, (value, key) => {
+			const render = RENDERERS[key]
+
+			// Return the column object, with formatted title, sortable and optional render function
+			return {
+				label: value,
+				field: key,
+				sortable: true,
+				render
+			}
+		})
 	}
 
 	generateTableData () {
-		return this.props.tail ? _.map(this.props.tail, (card) => {
-			const update = _.find(_.get(card, [ 'links', 'has attached element' ]), (linkedCard) => {
-				return [ 'update', 'update@1.0.0' ].includes(linkedCard.type)
+		return this.props.tail
+			? _.map(this.props.tail, (card) => {
+				const update = _.find(
+					_.get(card, [ 'links', 'has attached element' ]),
+					(linkedCard) => {
+						return [ 'update', 'update@1.0.0' ].includes(linkedCard.type)
+					}
+				)
+
+				// First we pick out the useful data
+				const pickedData = _.pick(card, [
+					'name',
+					'id',
+					'slug',
+					'created_at',
+					'data'
+				])
+
+				return {
+					...flatten(pickedData),
+					'Last updated': _.get(update, [ 'data', 'timestamp' ], null)
+				}
 			})
-			return {
-				name: card.name,
-				id: card.id,
-				slug: card.slug,
-				Created: card.created_at,
-				'Last updated': _.get(update, [ 'data', 'timestamp' ], null)
-			}
-		}) : null
+			: null
 	}
 
 	render () {
@@ -62,7 +88,9 @@ export default class CardTable extends BaseLens {
 			generateData,
 			columns
 		} = this.props
+		const tableColumns = columns || this.generateTableColumns()
 		const data = generateData ? generateData() : this.generateTableData()
+
 		return (
 			<Column overflowY flex="1">
 				<Box flex="1" style={{
@@ -72,7 +100,7 @@ export default class CardTable extends BaseLens {
 						<Table
 							rowKey={this.props.rowKey}
 							data={data}
-							columns={columns}
+							columns={tableColumns}
 							usePager={true}
 							itemsPerPage={PAGE_SIZE}
 							pagerPosition={'both'}
@@ -109,6 +137,5 @@ export default class CardTable extends BaseLens {
 }
 
 CardTable.defaultProps = {
-	rowKey: 'id',
-	columns: COLUMNS
+	rowKey: 'id'
 }
---
+++ apps/ui/lens/misc/Table/tests/helpers.spec.js
@@ -0,0 +1,166 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+const ava = require('ava')
+const helpers = require('../helpers')
+const account = require('./fixtures/account')
+const pullRequest = require('./fixtures/pull-request')
+
+ava('.flattenSchemaProperties should flatten all fields defined in the type schema', (test) => {
+	const result = helpers.flattenSchemaProperties(account)
+
+	const expectedResult = {
+		markers: {
+			type: 'array',
+			items: {
+				type: 'string', pattern: '^[a-zA-Z0-9-_/:+]+$'
+			}
+		},
+		name: {
+			type: 'string'
+		},
+		'data.type': {
+			default: 'Lead', enum: [ 'Lead', 'Customer' ]
+		},
+		'data.email': {
+			title: 'Email address',
+			type: 'array',
+			uniqueItems: true,
+			items: {
+				type: 'string', format: 'email'
+			}
+		},
+		'data.discountPercentage': {
+			title: 'Discount percentage', type: 'string'
+		},
+		'data.billingCode': {
+			title: 'Billing code', type: 'string'
+		},
+		'data.startsOnDate': {
+			title: 'Starts on date', type: 'string', format: 'date-time'
+		},
+		'data.endsOnDate': {
+			title: 'Ends on date', type: 'string', format: 'date-time'
+		},
+		'data.industry': {
+			title: 'Industry', type: 'string'
+		},
+		'data.location': {
+			title: 'Location', type: 'string'
+		},
+		'data.stageOfBusiness': {
+			title: 'Stage of business', type: 'string'
+		},
+		'data.description': {
+			title: 'Description', type: 'string', format: 'markdown'
+		}
+	}
+
+	test.deepEqual(result, expectedResult)
+})
+
+ava('.flattenSchemaProperties should flatten without losing property keys like titles', (test) => {
+	const result = helpers.flattenSchemaProperties(account)
+
+	const expectedResult = {
+		title: 'Discount percentage'
+	}
+
+	test.deepEqual(result['data.discountPercentage'].title, expectedResult.title)
+})
+
+ava('.flattenSchemaProperties should flatten without losing property keys like type', (test) => {
+	const result = helpers.flattenSchemaProperties(account)
+
+	const expectedResult = {
+		type: 'string'
+	}
+
+	test.deepEqual(result['data.discountPercentage'].type, expectedResult.type)
+})
+
+ava('.flattenSchemaProperties should return null when card doesn\t have a schema', (test) => {
+	const cardWithoutSchema = {
+		slug: 'account',
+		type: 'type@1.0.0',
+		name: 'Account',
+		version: '1.0.0',
+		markers: [],
+		tags: [],
+		links: {},
+		active: true,
+		data: {
+			meta: {
+				relationships: [
+					{
+						title: 'Opportunities',
+						link: 'has attached',
+						type: 'opportunity'
+					},
+					{
+						title: 'Contacts',
+						link: 'has',
+						type: 'contact'
+					},
+					{
+						title: 'Owner',
+						link: 'is owned by',
+						type: 'user'
+					},
+					{
+						title: 'Backup owners',
+						link: 'has backup owner',
+						type: 'user'
+					}
+				]
+			}
+		},
+		requires: [],
+		capabilities: []
+	}
+
+	const result = helpers.flattenSchemaProperties(cardWithoutSchema)
+
+	const expectedResult = null
+
+	test.deepEqual(result, expectedResult)
+})
+
+ava('.getTypeFields should return all fields on a card in a flattend object', (test) => {
+	const result = helpers.getTypeFields(pullRequest)
+
+	const expectedResult = {
+		name: 'name',
+		tags: 'tags',
+		'data.mentionsUser': 'data.mentionsUser',
+		'data.alertsUser': 'data.alertsUser',
+		'data.status': 'data.status',
+		'data.archived': 'data.archived',
+		'data.created_at': 'data.created_at',
+		'data.merged_at': 'data.merged_at',
+		'data.repository': 'data.repository',
+		'data.head.branch': 'data.head.branch',
+		'data.head.sha': 'data.head.sha',
+		'data.base.branch': 'data.base.branch',
+		'data.base.sha': 'data.base.sha'
+	}
+
+	test.deepEqual(result, expectedResult)
+})
+
+ava('.getTypeFields should return title of flattend object when title is defined', (test) => {
+	const result = helpers.getTypeFields(account)
+
+	const expectedResult = {
+		title: 'Discount percentage'
+	}
+
+	console.log(result)
+
+	// Console.log(account.data.schema.properties.data)
+
+	test.deepEqual(result['data.discountPercentage'], expectedResult.title)
+})
