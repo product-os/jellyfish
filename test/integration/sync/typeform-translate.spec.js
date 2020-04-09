6b200bb8f0d95d5654167b6e7077a95384366cf7
Marios Balamatsias <mbalamatsias@gmail.com>
2020-04-09 23:51:18 +0300

    test/integration/sync: Enable typeform tests
    Change-type: patch
    Signed-off-by: Marios Balamatsias <mbalamatsias@gmail.com>

---
+++ test/integration/sync/typeform-translate.spec.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+const ava = require('ava')
+const scenario = require('./scenario')
+
+ava.serial.beforeEach(scenario.beforeEach)
+ava.serial.afterEach.always(scenario.afterEach)
+
+scenario.run(ava, {
+	integration: require('../../../lib/sync/integrations/typeform'),
+	scenarios: require('./webhooks/typeform'),
+	baseUrl: 'https://api.typeform.com',
+	stubRegex: /.*/,
+	source: 'typeform',
+	// eslint-disable-next-line lodash/prefer-constant
+	isAuthorized: () => {
+		return true
+	}
+})
