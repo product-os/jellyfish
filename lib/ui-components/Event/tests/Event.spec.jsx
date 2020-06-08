1e54bb6a83041a91c4b47ceaf8a64736b5e00c5c
Graham McCulloch <admin@grahammcculloch.co.uk>
2020-06-08 09:42:15 +0700

    Add unit test for editing a message
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- lib/ui-components/Event/tests/Event.spec.jsx
+++ lib/ui-components/Event/tests/Event.spec.jsx
@@ -199,3 +199,77 @@ ava('A markdown message is displayed when the card is a message', async (test) =
 	const message = event.find(Markdown)
 	test.is(message.text().trim(), messageText)
 })
+
+ava('Editing a message will update the mentions, alerts, tags and message', async (test) => {
+	const author = {
+		...user,
+		id: card.data.actor
+	}
+	const mentionSlug = 'john'
+	const alertSlug = 'paul'
+	const tag = 'ringo'
+	const newMessage = `Test @${mentionSlug} !${alertSlug} #${tag}`
+	const expectedPatchSet = new Set([
+		{
+			op: 'add', path: '/tags/0', value: tag
+		},
+		{
+			op: 'add',
+			path: '/data/payload/mentionsUser/0',
+			value: `user-${mentionSlug}`
+		},
+		{
+			op: 'add', path: '/data/payload/alertsUser/0', value: `user-${alertSlug}`
+		},
+		{
+			op: 'replace',
+			path: '/data/payload/message',
+			value: newMessage
+		}
+	])
+
+	const onUpdateCard = sandbox.stub().resolves(null)
+
+	// HACK to get react-textarea-autosize not to complain
+	// eslint-disable-next-line no-multi-assign
+	global.getComputedStyle = global.window.getComputedStyle = () => {
+		return {
+			height: '100px',
+			getPropertyValue: (name) => {
+				return name === 'box-sizing' ? '' : null
+			}
+		}
+	}
+
+	const event = await mount(
+		<Event
+			{...commonProps}
+			onUpdateCard={onUpdateCard}
+			user={author}
+			card={card}
+		/>, {
+			wrappingComponent: wrapper
+		}
+	)
+
+	// Go into edit mode
+	event.find('button[data-test="event-header__context-menu-trigger"]').simulate('click')
+	event.find('a[data-test="event-header__link--edit-message"]').simulate('click')
+	const autocomplete = event.find('AutoCompleteArea')
+
+	// Force the change via the props (avoid interaction with react-textarea-autosize)
+	autocomplete.props().onChange({
+		target: {
+			value: newMessage
+		}
+	})
+	autocomplete.props().onSubmit()
+
+	// Verify the onUpdateCard prop callback is called with the expected patch
+	test.is(onUpdateCard.callCount, 1)
+	test.is(onUpdateCard.getCall(0).args[0].id, card.id)
+
+	// Use a Set here as we can't be sure of the order of patches in the patch array
+	const updatePatchSet = new Set(onUpdateCard.getCall(0).args[1])
+	test.deepEqual(updatePatchSet, expectedPatchSet)
+})
