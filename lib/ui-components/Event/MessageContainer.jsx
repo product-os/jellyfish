19565879cb7a1b8d7dd06adaa947aa750d1372ca
Graham McCulloch <admin@grahammcculloch.co.uk>
2020-06-17 15:17:37 +0700

    Fix coloring of message when editing a whisper
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- lib/ui-components/Event/MessageContainer.jsx
+++ lib/ui-components/Event/MessageContainer.jsx
@@ -44,7 +44,11 @@ const MessageContainer = styled(Box) `
 		card, actor, theme, editing
 	}) => {
 		if (editing) {
-			return `
+			return (card.type === 'whisper' || card.type === 'whisper@1.0.0') ? `
+				border: solid 0.5px ${theme.colors.tertiary.light};
+				background: #2E587ADD;
+				color: white;
+			` : `
 				border: solid 0.5px ${theme.colors.gray.main};
 				background: ${theme.colors.gray.light};
 				color: ${theme.colors.text.main};
