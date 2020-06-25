65cabd9c3c89f015ad466a0d50de68411e38ecfd
Josh Bowling <josh@balena.io>
2020-06-25 15:54:45 +0900

    Switch to third-party library for UUID validation
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- test/unit/uuid/index.spec.js
+++ test/unit/uuid/index.spec.js
@@ -8,14 +8,6 @@ const ava = require('ava')
 const _ = require('lodash')
 const uuid = require('../../../lib/uuid')
 
-ava('.isUUID() should return true for a uuid', async (test) => {
-	test.true(uuid.isUUID(await uuid.random()))
-})
-
-ava('.isUUID() should return false if input is not a complete uuid', async (test) => {
-	test.false(uuid.isUUID((await uuid.random()).slice(0, 10)))
-})
-
 ava('REGEX should be a regular expression', (test) => {
 	test.true(_.isRegExp(uuid.REGEX))
 })
