851728de75a9d28529ca3250dce35db55246c7e7
Josh Bowling <josh@balena.io>
2020-07-28 11:35:50 +0900

    Replace lib/sync with private package
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- test/integration/server/oauth.spec.js
+++ test/integration/server/oauth.spec.js
@@ -14,7 +14,7 @@ const {
 const environment = require('@balena/jellyfish-environment')
 const {
 	slugify
-} = require('../../../lib/sync/integrations/utils')
+} = require('@balena/jellyfish-sync/lib/integrations/utils')
 const helpers = require('./helpers')
 
 const outreachTest =
