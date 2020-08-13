968a109885a0cf03ae5adb19df9222921a40f93f
Marios Balamatsias <mbalamatsias@gmail.com>
2020-08-13 16:57:11 +0300

    test/e2e/ui: Add e2e test for pull-requests
    Change-type: patch
    Signed-off-by: Marios Balamatsias <mbalamatsias@gmail.com>

---
+++ test/e2e/ui/pull-requests.spec.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+const ava = require('ava')
+const _ = require('lodash')
+const {
+	v4: uuid
+} = require('uuid')
+const helpers = require('./helpers')
+const macros = require('./macros')
+
+const context = {
+	context: {
+		id: `UI-INTEGRATION-TEST-${uuid()}`
+	}
+}
+
+const user = {
+	username: `johndoe-${uuid()}`,
+	email: `johndoe-${uuid()}@example.com`,
+	password: 'password'
+}
+
+ava.serial.before(async () => {
+	await helpers.browser.beforeEach({
+		context
+	})
+
+	// Create user and log in to the web browser client
+	context.communityUser = await context.createUser(user)
+	await context.addUserToBalenaOrg(context.communityUser.id)
+	await macros.loginUser(context.page, user)
+})
+
+ava.serial.afterEach.always(async (test) => {
+	await helpers.afterEach({
+		context, test
+	})
+})
+
+ava.serial.after.always(async () => {
+	await helpers.browser.afterEach({
+		context
+	})
+})
+
+ava.serial('Should be able to navigate to chart lens of pull requests', async (test) => {
+	const {
+		page
+	} = context
+
+	await macros.navigateToHomeChannelItem(page, [
+		'[data-test="home-channel__group-toggle--org-balena"]',
+		'[data-test="home-channel__item--view-all-pull-requests"]'
+	])
+
+	const pullRequests = require('./fixtures/pull-requests.json')
+	_.forEach(pullRequests, async (pullRequest) => {
+		await page.evaluate((data) => {
+			return window.sdk.card.create({
+				type: 'pull-request@1.0.0',
+				data
+			})
+		}, pullRequest)
+	})
+	await macros.waitForThenClickSelector(page, '[data-test="lens-selector--lens-chart"]')
+	await page.waitForSelector('.plotly')
+	test.pass()
+})
