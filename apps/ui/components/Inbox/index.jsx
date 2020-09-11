6c89c8a8c208917a7d8e4257a1ce2a362c6a2674
Graham McCulloch <admin@grahammcculloch.co.uk>
2020-09-11 08:22:45 +0700

    Separate presentational and HOC components
    Easier for unit testing!
    
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- apps/ui/components/Inbox/index.jsx
+++ apps/ui/components/Inbox/index.jsx
@@ -6,90 +6,15 @@
 
 import _ from 'lodash'
 import {
-	circularDeepEqual
-} from 'fast-equals'
-import styled from 'styled-components'
-import {
 	connect
 } from 'react-redux'
-import React, {
-	useState
-} from 'react'
-import {
-	Flex,
-	Heading,
-	Tabs,
-	Tab
-} from 'rendition'
 import {
 	bindActionCreators
 } from 'redux'
-import Column from '@balena/jellyfish-ui-components/lib/shame/Column'
 import {
 	actionCreators
 } from '../../core'
-import InboxTab from './InboxTab'
-import * as queries from './queries'
-
-const InboxColumn = styled(Column) `
-	[role="tabpanel"] {
-		display: flex;
-	  flex-direction: column;
-	  height: 100vh;
-	}
-`
-
-const Inbox = React.memo(({
-	setupStream,
-	paginateStream
-}) => {
-	// State controller for managing the active tab
-	const [ currentTab, setCurrentTab ] = useState(0)
-
-	const defaultTabProps = {
-		setupStream,
-		paginateStream,
-		currentTab,
-		key: currentTab
-	}
-
-	return (
-		<InboxColumn>
-			<Flex p={3} justifyContent="space-between">
-				<Heading.h4>
-					Inbox
-				</Heading.h4>
-			</Flex>
-
-			<Tabs
-				activeIndex={currentTab}
-				onActive={setCurrentTab}
-			>
-				<Tab title="Unread">
-					<InboxTab
-						{ ...defaultTabProps }
-						getQuery={queries.getUnreadQuery}
-						canMarkAsRead
-					/>
-				</Tab>
-
-				<Tab title="Read">
-					<InboxTab
-						{ ...defaultTabProps }
-						getQuery={queries.getReadQuery}
-					/>
-				</Tab>
-
-				<Tab title="Sent">
-					<InboxTab
-						{ ...defaultTabProps }
-						getQuery={queries.getSentQuery}
-					/>
-				</Tab>
-			</Tabs>
-		</InboxColumn>
-	)
-}, circularDeepEqual)
+import Inbox from './Inbox'
 
 const mapDispatchToProps = (dispatch) => {
 	return bindActionCreators(
