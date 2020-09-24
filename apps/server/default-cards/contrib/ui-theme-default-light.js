68927c743a464a4fee2fbfbc2a7ce0431ad0c938
Graham McCulloch <admin@grahammcculloch.co.uk>
2020-09-24 18:30:10 +0700

    Add default ui-theme cards
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

---
+++ apps/server/default-cards/contrib/ui-theme-default-light.js
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+module.exports = {
+	name: 'Default Light',
+	slug: 'ui-theme-default-light',
+	type: 'ui-theme@1.0.0',
+	markers: [],
+	data: {
+		gray: {
+			dark: '#9f9f9f',
+			main: '#c6c8c9',
+			light: '#f4f4f4',
+			semilight: '#fff'
+		},
+		info: {
+			dark: '#107dbc',
+			main: '#1496e1',
+			light: '#e8f5fc',
+			semilight: '#90cbee'
+		},
+		text: {
+			dark: '#23445e',
+			main: '#2a506f',
+			light: '#527699',
+			semilight: '#fff'
+		},
+		danger: {
+			dark: '#eb0800',
+			main: '#ff423d',
+			light: '#feebeb',
+			semilight: '#ffa1a1'
+		},
+		primary: {
+			dark: '#009dd7',
+			main: '#00aeef',
+			light: '#08bcff',
+			semilight: '#aedff9'
+		},
+		success: {
+			dark: '#138b27',
+			main: '#1ac135',
+			light: '#e8f8ea',
+			semilight: '#8ce09a'
+		},
+		warning: {
+			dark: '#ad6800',
+			main: '#fca321',
+			light: '#fef3e5',
+			semilight: '#fdd190'
+		},
+		tertiary: {
+			dark: '#456482',
+			main: '#527699',
+			light: '#5b82a7',
+			semilight: '#bbc8d6'
+		},
+		secondary: {
+			dark: '#23445e',
+			main: '#2a506f',
+			light: '#2e587a',
+			semilight: '#abb9c5'
+		},
+		background: {
+			dark: '#ffffff',
+			main: '#fff',
+			light: '#fff',
+			semilight: '#fff'
+		},
+		quartenary: {
+			dark: '#b7bed3',
+			main: '#dde1f0',
+			light: '#f8f9fd',
+			semilight: '#fff'
+		},
+		description: 'The default Jellyfish theme',
+		screenshots: []
+	}
+}
