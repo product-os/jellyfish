5b7a4ef7f64f3a896e22e7b38fd3f234c142fd4c
Josh Bowling <josh@balena.io>
2020-10-14 04:48:58 +0000

    Add missing license header
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- test/unit/apps/ui/fixtures/types/index.js
+++ test/unit/apps/ui/fixtures/types/index.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
 const user = require('./user.json')
 const org = require('./org.json')
 const contact = require('./contact.json')
