01e79569721796df4a2c90812d916041e1ad375c
Lucy-Jane <ljewalsh@gmail.com>
2020-10-27 11:50:41 +0100

    Add test to confirm the 'user-community' role is added to a user when send-first-time-time-login action is fired and their roles field contains a value that is not 'user-community'
    Change-type: patch
    Signed-off-by: Lucy-Jane Walsh <lucy-jane@balena.io>

--- test/integration/worker/actions/send-first-time-login-link.spec.js
+++ test/integration/worker/actions/send-first-time-login-link.spec.js
@@ -752,3 +752,61 @@ ava('a community role is added to a supplied user with no role set', async (test
 
 	test.deepEqual(updatedUser.data.roles, [ 'user-community' ])
 })
+
+ava('a community role is added to a supplied user when it is not present in the roles field', async (test) => {
+	const {
+		session,
+		context,
+		processAction,
+		userCard,
+		org,
+		jellyfish,
+		worker,
+		nockRequest
+	} = test.context
+
+	nockRequest()
+
+	const createUserAction = await worker.pre(session, {
+		action: 'action-create-card@1.0.0',
+		context,
+		card: userCard.id,
+		type: userCard.type,
+		arguments: {
+			reason: 'for testing',
+			properties: {
+				slug: test.context.generateRandomSlug({
+					prefix: 'user'
+				}),
+				data: {
+					hash: 'fake-hash',
+					email: 'fake@email.com',
+					roles: [ 'user-external-support' ]
+				}
+			}
+		}
+	})
+
+	const userWithoutRole = await processAction(session, createUserAction)
+	test.false(userWithoutRole.error)
+
+	const linkAction = await createOrgLinkAction({
+		toId: userWithoutRole.data.id,
+		fromId: org.data.id,
+		context
+	})
+
+	await processAction(session, linkAction)
+
+	await processAction(session, {
+		action: 'action-send-first-time-login-link@1.0.0',
+		context,
+		card: userWithoutRole.data.id,
+		type: userWithoutRole.data.type,
+		arguments: {}
+	})
+
+	const updatedUser = await jellyfish.getCardById(context, session, userWithoutRole.data.id)
+
+	test.deepEqual(updatedUser.data.roles, [ 'user-external-support', 'user-community' ])
+})
