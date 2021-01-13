102ae8c7afd8c612a587ac7eed02e5e377c0f4c3
StefKors <stef.kors@gmail.com>
2021-01-13 15:43:29 +0100

    Remove getLinks from Segment component

--- apps/ui/lib/lens/common/Segment.jsx
+++ apps/ui/lib/lens/common/Segment.jsx
@@ -4,11 +4,6 @@
  * Proprietary and confidential.
  */
 
-import Bluebird from 'bluebird'
-import clone from 'deep-copy'
-import {
-	circularDeepEqual
-} from 'fast-equals'
 import _ from 'lodash'
 import React from 'react'
 import {
@@ -31,31 +26,12 @@ class Segment extends React.Component {
 		super(props)
 
 		this.state = {
-			results: props.draftCards || null,
 			showLinkModal: false
 		}
 
 		this.openCreateChannel = this.openCreateChannel.bind(this)
 		this.openLinkModal = this.openLinkModal.bind(this)
 		this.hideLinkModal = this.hideLinkModal.bind(this)
-		this.updateResults = this.updateResults.bind(this)
-		this.getData = this.getData.bind(this)
-	}
-
-	componentDidUpdate (prevProps) {
-		if (
-			!circularDeepEqual(prevProps.segment, this.props.segment)
-		) {
-			this.setState({
-				results: null
-			})
-			this.getData()
-		} else if (
-			!circularDeepEqual(prevProps.card, this.props.card) ||
-			!circularDeepEqual(prevProps.draftCards, this.props.draftCards)
-		) {
-			this.getData()
-		}
 	}
 
 	openLinkModal () {
@@ -68,58 +44,6 @@ class Segment extends React.Component {
 		this.setState({
 			showLinkModal: false
 		})
-	}
-
-	componentDidMount () {
-		this.getData()
-	}
-
-	updateResults (results) {
-		const {
-			onCardsUpdated,
-			draftCards
-		} = this.props
-		this.setState({
-			results: _.unionBy(results, (draftCards || []), 'id')
-		}, () => {
-			if (onCardsUpdated) {
-				onCardsUpdated(results)
-			}
-		})
-	}
-
-	async getData () {
-		const {
-			card,
-			segment,
-			actions,
-			sdk
-		} = this.props
-
-		if (segment.link) {
-			const results = await	actions.getLinks({
-				sdk
-			}, card, segment.link)
-			this.updateResults(results)
-		} else if (segment.query) {
-			let context = [ card ]
-			for (const relation of _.castArray(segment.query)) {
-				if (relation.link) {
-					context = actions.getLinks({
-						sdk
-					}, card, relation.link)
-				} else {
-					const mapped = await Bluebird.map(context, (item) => {
-						return actions.queryAPI(helpers.evalSchema(clone(relation), {
-							result: item
-						}))
-					})
-					context = _.flatten(mapped)
-				}
-			}
-
-			this.updateResults(_.flatten(context))
-		}
 	}
 
 	openCreateChannel () {
@@ -139,11 +63,6 @@ class Segment extends React.Component {
 				}),
 				seed: {
 					markers: card.markers
-				},
-				onDone: {
-					action: 'link',
-					targets: [ card ],
-					callback: this.getData
 				}
 			},
 			format: 'create',
@@ -153,11 +72,11 @@ class Segment extends React.Component {
 
 	render () {
 		const {
-			results,
 			showLinkModal
 		} = this.state
 
 		const {
+			draftCards,
 			actions,
 			card,
 			segment,
@@ -165,6 +84,9 @@ class Segment extends React.Component {
 			onSave,
 			showLinkToExistingElementButton = true
 		} = this.props
+
+		const linkedCards = _.get(card, [ 'links', segment.link ], draftCards)
+		const results = _.unionBy(linkedCards, (draftCards || []), 'id')
 
 		const type = _.find(types, {
 			slug: helpers.getRelationshipTargetType(segment)
@@ -236,7 +158,6 @@ class Segment extends React.Component {
 						types={[ type ]}
 						onHide={this.hideLinkModal}
 						onSave={onSave}
-						onSaved={this.getData}
 					/>
 				)}
 			</Flex>
