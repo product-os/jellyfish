a44f1ff06de799638b00858c73de8626c0a317e2
Josh Bowling <josh@balena.io>
2021-02-02 13:03:46 +0900

    Add pg_sleep back in, use FOR UPDATE SKIP LOCKED

--- copy-create-date.sql
+++ copy-create-date.sql
@@ -4,7 +4,7 @@ DECLARE
 	total INTEGER := 0;
 BEGIN
 	LOOP
-		UPDATE cards SET new_created_at=TO_TIMESTAMP(created_at, 'YYYY-MM-DDThh24:mi:ss.MSZ') WHERE id IN (SELECT id FROM cards WHERE new_created_at IS NULL LIMIT 100);
+		UPDATE cards SET new_created_at=TO_TIMESTAMP(created_at, 'YYYY-MM-DDThh24:mi:ss.MSZ') WHERE id IN (SELECT id FROM cards WHERE new_created_at IS NULL FOR UPDATE SKIP LOCKED LIMIT 1000);
 		COMMIT;
 		GET DIAGNOSTICS updated = ROW_COUNT;
 		IF updated = 0 THEN
@@ -12,6 +12,7 @@ BEGIN
 		END IF;
 		total = total + updated;	
 		RAISE INFO 'Updated % rows...', total;
+		PERFORM pg_sleep(1);
 	END LOOP;
 END;
 $$ LANGUAGE plpgsql;
