0b7926e043e945a289a3502b22ec865869fbe680
Josh Bowling <josh@balena.io>
2021-02-09 11:12:28 +0900

    Set no timeout, fix total addition placement

--- copy-create-date.sql
+++ copy-create-date.sql
@@ -1,3 +1,4 @@
+SET statement_timeout=0;
 DO $$
 DECLARE
 	updated INTEGER := 0;
@@ -6,12 +7,12 @@ BEGIN
 	LOOP
 		UPDATE cards SET new_created_at=TO_TIMESTAMP(created_at, 'YYYY-MM-DDThh24:mi:ss.MSZ') WHERE id IN (SELECT id FROM cards WHERE new_created_at IS NULL FOR UPDATE SKIP LOCKED LIMIT 1000);
 		COMMIT;
-		IF NOT EXISTS (SELECT FROM cards WHERE new_created_at IS NULL) THEN
-			EXIT;
-		END IF;
 		GET DIAGNOSTICS updated = ROW_COUNT;
 		total = total + updated;
 		RAISE INFO 'Updated % rows...', total;
+		IF NOT EXISTS (SELECT FROM cards WHERE new_created_at IS NULL) THEN
+			EXIT;
+		END IF;
 		PERFORM pg_sleep(1);
 	END LOOP;
 END;
--- copy-update-date.sql
+++ copy-update-date.sql
@@ -1,3 +1,4 @@
+SET statement_timeout=0;
 DO $$
 DECLARE
 	updated INTEGER := 0;
@@ -6,12 +7,12 @@ BEGIN
 	LOOP
 		UPDATE cards SET new_updated_at=TO_TIMESTAMP(updated_at, 'YYYY-MM-DDThh24:mi:ss.MSZ') WHERE id IN (SELECT id FROM cards WHERE updated_at IS NOT NULL AND new_updated_at IS NULL FOR UPDATE SKIP LOCKED LIMIT 1000);
 		COMMIT;
-		IF NOT EXISTS (SELECT FROM cards WHERE updated_at IS NOT NULL AND new_updated_at IS NULL) THEN
-			EXIT;
-		END IF;
 		GET DIAGNOSTICS updated = ROW_COUNT;
 		total = total + updated;
 		RAISE INFO 'Updated % rows...', total;
+		IF NOT EXISTS (SELECT FROM cards WHERE updated_at IS NOT NULL AND new_updated_at IS NULL) THEN
+			EXIT;
+		END IF;
 		PERFORM pg_sleep(1);
 	END LOOP;
 END;
