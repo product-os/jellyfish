9df1eb8b257f333e038ebf61fc4e8682e49a563f
Graham McCulloch <admin@grahammcculloch.co.uk>
2021-02-10 10:32:46 +0700

    Make use of channels plugin
    Manually bootstrap channels as part of the server's bootstrap process.
    
    Change-type: minor
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- apps/server/lib/plugins.js
+++ apps/server/lib/plugins.js
@@ -8,9 +8,13 @@ const logger = require('@balena/jellyfish-logger').getLogger(__filename)
 const {
 	PluginManager
 } = require('@balena/jellyfish-plugin-base')
+const {
+	ChannelsPlugin
+} = require('@balena/jellyfish-plugin-channels')
 const ActionLibrary = require('@balena/jellyfish-action-library')
 const DefaultPlugin = require('@balena/jellyfish-plugin-default')
 const ProductOSPlugin = require('@balena/jellyfish-plugin-product-os')
+const TypeformPlugin = require('@balena/jellyfish-plugin-typeform')
 
 exports.getPluginManager = (context) => {
 	logger.info(context, 'Loading plugins')
@@ -18,7 +22,9 @@ exports.getPluginManager = (context) => {
 		plugins: [
 			ActionLibrary,
 			DefaultPlugin,
-			ProductOSPlugin
+			ChannelsPlugin,
+			ProductOSPlugin,
+			TypeformPlugin
 		]
 	})
 }
