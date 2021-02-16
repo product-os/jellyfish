c857d406d30fa9771f726c1febf8f8ef762a7e72
Josh Bowling <josh@balena.io>
2021-02-16 11:10:22 +0900

    tmp

--- migrate-date-columns.sql
+++ migrate-date-columns.sql
@@ -1,21 +1,25 @@
--- Lock tables.
-LOCK TABLE cards IN SHARE MODE;
-LOCK TABLE links IN SHARE MODE;
+DO $$
+	BEGIN
+		-- Lock tables.
+		LOCK TABLE cards IN SHARE MODE;
+		LOCK TABLE links IN SHARE MODE;
 
--- Disable all triggers, namely for streams.
-ALTER TABLE cards DISABLE TRIGGER ALL;
+		-- Disable all triggers, namely for streams.
+		ALTER TABLE cards DISABLE TRIGGER ALL;
 
--- Rename columns.
-ALTER TABLE cards RENAME COLUMN created_at TO old_created_at;
-ALTER TABLE cards RENAME COLUMN updated_at TO old_updated_at;
-ALTER TABLE cards RENAME COLUMN new_created_at TO created_at;
-ALTER TABLE cards RENAME COLUMN new_updated_at TO updated_at;
+		-- Rename columns.
+		ALTER TABLE cards RENAME COLUMN created_at TO old_created_at;
+		ALTER TABLE cards RENAME COLUMN updated_at TO old_updated_at;
+		ALTER TABLE cards RENAME COLUMN new_created_at TO created_at;
+		ALTER TABLE cards RENAME COLUMN new_updated_at TO updated_at;
 
--- Rename indexes.
-ALTER INDEX IF EXISTS created_at_cards_idx RENAME to old_created_at_cards_idx;
-ALTER INDEX IF EXISTS updated_at_cards_idx RENAME to old_updated_at_cards_idx;
-ALTER INDEX IF EXISTS new_created_at_cards_idx RENAME to created_at_cards_idx;
-ALTER INDEX IF EXISTS new_updated_at_cards_idx RENAME to updated_at_cards_idx;
+		-- Rename indexes.
+		ALTER INDEX IF EXISTS created_at_cards_idx RENAME to old_created_at_cards_idx;
+		ALTER INDEX IF EXISTS updated_at_cards_idx RENAME to old_updated_at_cards_idx;
+		ALTER INDEX IF EXISTS new_created_at_cards_idx RENAME to created_at_cards_idx;
+		ALTER INDEX IF EXISTS new_updated_at_cards_idx RENAME to updated_at_cards_idx;
 
--- Re-enable triggers.
-ALTER TABLE cards ENABLE TRIGGER ALL;
+		-- Re-enable triggers.
+		ALTER TABLE cards ENABLE TRIGGER ALL;
+	END;
+$$ LANGUAGE plpgsql;
--- rollback-migrate-date-columns.sql
+++ rollback-migrate-date-columns.sql
@@ -1,17 +1,21 @@
--- Disable all triggers, namely for streams.
-ALTER TABLE cards DISABLE TRIGGER ALL;
+DO $$
+	BEGIN
+		-- Disable all triggers, namely for streams.
+		ALTER TABLE cards DISABLE TRIGGER ALL;
 
--- Rename columns back to original state.
-ALTER TABLE cards RENAME COLUMN created_at TO new_created_at;
-ALTER TABLE cards RENAME COLUMN updated_at TO new_updated_at;
-ALTER TABLE cards RENAME COLUMN old_created_at TO created_at;
-ALTER TABLE cards RENAME COLUMN old_updated_at TO updated_at;
+		-- Rename columns back to original state.
+		ALTER TABLE cards RENAME COLUMN created_at TO new_created_at;
+		ALTER TABLE cards RENAME COLUMN updated_at TO new_updated_at;
+		ALTER TABLE cards RENAME COLUMN old_created_at TO created_at;
+		ALTER TABLE cards RENAME COLUMN old_updated_at TO updated_at;
 
--- Rename indexes back to original state.
-ALTER INDEX IF EXISTS created_at_cards_idx RENAME TO new_created_at_cards_idx;
-ALTER INDEX IF EXISTS updated_at_cards_idx RENAME TO new_updated_at_cards_idx;
-ALTER INDEX IF EXISTS old_created_at_cards_idx RENAME TO created_at_cards_idx;
-ALTER INDEX IF EXISTS old_updated_at_cards_idx RENAME TO updated_at_cards_idx;
+		-- Rename indexes back to original state.
+		ALTER INDEX IF EXISTS created_at_cards_idx RENAME TO new_created_at_cards_idx;
+		ALTER INDEX IF EXISTS updated_at_cards_idx RENAME TO new_updated_at_cards_idx;
+		ALTER INDEX IF EXISTS old_created_at_cards_idx RENAME TO created_at_cards_idx;
+		ALTER INDEX IF EXISTS old_updated_at_cards_idx RENAME TO updated_at_cards_idx;
 
--- Re-enable triggers.
-ALTER TABLE cards ENABLE TRIGGER ALL;
+		-- Re-enable triggers.
+		ALTER TABLE cards ENABLE TRIGGER ALL;
+	END;
+$$ LANGUAGE plpgsql;
