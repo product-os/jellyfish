fbffb4e5db202285f8dd445db6edc2bd7413f814
Graham McCulloch <admin@grahammcculloch.co.uk>
2021-03-04 14:01:30 +0700

    AutoCompleteCardSelect provides it's own types prop now
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

--- apps/ui/lib/components/AutoCompleteCardSelect/index.js
+++ apps/ui/lib/components/AutoCompleteCardSelect/index.js
@@ -4,9 +4,27 @@
  * Proprietary and confidential.
  */
 
+import {
+	connect
+} from 'react-redux'
+import {
+	compose
+} from 'redux'
+import {
+	selectors
+} from '../../core'
 import AutoCompleteCardSelect from './AutoCompleteCardSelect'
 import {
 	withSetup
 } from '@balena/jellyfish-ui-components'
 
-export default withSetup(AutoCompleteCardSelect)
+const mapStateToProps = (state) => {
+	return {
+		types: selectors.getTypes(state)
+	}
+}
+
+export default compose(
+	withSetup,
+	connect(mapStateToProps)
+)(AutoCompleteCardSelect)
