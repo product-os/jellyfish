70f7ed37aedba603a462c3f3c77140852055687e
ab77 <anton@balena.io>
2021-07-05 15:27:15 -0700

    JF on balenaCloud
    * dynamically generate PKI assets and internal secrets
    * set HAProxy to balena upstream
    * HTTPS (h2) everywhere
    * Update docs, fix/refactor scripts, remove cruft
    * add tests container
    * remove from resinCI
    * remove k8s assets
    * move AWS templates
    * clean-up and re-encrypt git-secrets
    * (WIP) deploy to balenaCloud with GH action
    * (WIP) add MinIO/S3
    
    Change-type: major

---
+++ scripts/lint/check-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###
+# Copyright (C) Balena.io - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+# Proprietary and confidential.
+###
+
+set -eu
+
+function assert_no_tests_in_directory () (
+	echo "Checking that there are no tests in $1"
+	if [ -d "$1" ]; then
+		tests="$(find "$1" -name '*.spec.*' -and -not -path "$1/node_modules/*")"
+		if [ -n "$tests" ]; then
+			echo "No test files in $1 are allowed:" 1>&2
+			echo "" 1>&2
+			echo "$tests" 1>&2
+			exit 1
+		fi
+	fi
+)
+
+UI_COMPONENTS="ui sdk"
+for component in $UI_COMPONENTS; do
+	assert_no_tests_in_directory "test/unit/$component"
+done
