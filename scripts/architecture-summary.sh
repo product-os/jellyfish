707ef77e4fd8cf081d6939bcce3ad36f3e73f032
ab77 <anton@balena.io>
2021-07-05 15:27:15 -0700

    JF on balenaCloud
    * dynamically generate PKI assets and internal secrets
    * set HAProxy to balena upstream
    * HTTPS (h2) everywhere
    * Update docs, fix/refactor scripts, remove cruft
    * add tests container
    * remove from resinCI
    * remove k8s assets
    * move AWS templates
    * clean-up and re-encrypt git-secrets
    * (WIP) deploy to balenaCloud with GH action
    * (WIP) add MinIO/S3
    
    Change-type: major

--- scripts/architecture-summary.sh
+++ scripts/architecture-summary.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+###
+# Copyright (C) Balena.io - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+# Proprietary and confidential.
+###
+
 set -eu
 
 APPS="$(find apps -maxdepth 1 -mindepth 1 -type d | sort -g)"
