32fa788621c1015be759c971f296c6beccf45955
Lucian Buzzo <lucian.buzzo@gmail.com>
2021-08-17 15:55:18 +0000

    Add a simple UI for check-runs
    This commit adds a basic UI for check runs that shows all of the
    contracts created by transformers from the commit the check run is
    attached to. The graph is generated using mermaidJS, so doesn't add any
    additional dependencies to the system. In the future I'd like to develop
    this concept into a generic graph visualisation features, however there
    are behavioural issues that need to be resolved, such as which links you
    expand/display and to what depth.
    
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- apps/ui/jest.config.js
+++ apps/ui/jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
 		env: {}
 	},
 	transform: {
+		'node_modules/entity-decode/(.*)': 'jest-esm-transformer',
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$':
 			'<rootDir>/file-transformer.js'
 	}
