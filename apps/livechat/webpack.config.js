8a7282235c915754be060c0fd156aff7687ba3e7
Lucian Buzzo <lucian.buzzo@gmail.com>
2021-10-07 14:15:00 +0100

    Switch to esbuild loader for faster UI bundling
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- apps/livechat/webpack.config.js
+++ apps/livechat/webpack.config.js
@@ -36,8 +36,16 @@ const config = {
 		rules: [
 			{
 				test: /\.(ts|tsx)?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'esbuild-loader',
+						options: {
+							loader: 'tsx',
+							target: 'es2015'
+						}
+					}
+				]
 			},
 			{
 				test: /\.css$/,
