e55e4b6d30c79120980a873c29ae416263938dc9
Martin Rauscher <martin@balena.io>
2021-10-25 14:31:03 +0200

    update worker and core deps
    Change-type: patch
    Signed-off-by: Martin Rauscher <martin@balena.io>

--- test/e2e/server/transformers.spec.js
+++ test/e2e/server/transformers.spec.js
@@ -4,30 +4,34 @@
  * Proprietary and confidential.
  */
 
-const ava = require('ava');
-const { v4: uuid } = require('uuid');
-const helpers = require('../sdk/helpers');
+const ava = require('ava')
+const {
+	v4: uuid
+} = require('uuid')
+const helpers = require('../sdk/helpers')
 
-ava.serial.before(helpers.before);
-ava.serial.after.always(helpers.after);
+ava.serial.before(helpers.before)
+ava.serial.after.always(helpers.after)
 
-ava.serial.beforeEach(helpers.beforeEach);
-ava.serial.afterEach.always(helpers.afterEach);
+ava.serial.beforeEach(helpers.beforeEach)
+ava.serial.afterEach.always(helpers.afterEach)
 
 const createUserDetails = () => {
 	return {
 		username: uuid(),
 		email: `${uuid()}@example.com`,
-		password: 'foobarbaz',
-	};
-};
+		password: 'foobarbaz'
+	}
+}
 
 ava.serial(
 	'transformer properties should evaluate with formulas and triggers',
 	async (test) => {
-		const { sdk } = test.context;
+		const {
+			sdk
+		} = test.context
 
-		const user1Details = createUserDetails();
+		const user1Details = createUserDetails()
 
 		// Create user 1 and login as them
 		await sdk.action({
@@ -37,64 +41,64 @@ ava.serial(
 			arguments: {
 				username: `user-${user1Details.username}`,
 				email: user1Details.email,
-				password: user1Details.password,
-			},
-		});
+				password: user1Details.password
+			}
+		})
 
-		await sdk.auth.login(user1Details);
+		await sdk.auth.login(user1Details)
 
-		await sdk.auth.whoami();
+		await sdk.auth.whoami()
 
 		const src1 = await sdk.card.create({
 			type: 'service-source@1.0.0',
 			data: {
-				logme: true,
-			},
-		});
+				logme: true
+			}
+		})
 
 		const src2 = await sdk.card.create({
 			type: 'service-source@1.0.0',
 			data: {
-				logme: true,
-			},
-		});
-		await sdk.card.link(src1, src2, 'was built into');
+				logme: true
+			}
+		})
+		await sdk.card.link(src1, src2, 'was built into')
 
-		console.log(`testing contracts ${src1.id} -[was built into-> ${src2.id}`);
+		console.log(`testing contracts ${src1.id} -[was built into-> ${src2.id}`)
 
 		const src1final = await sdk.card.create({
-			type: 'service-source@1.0.0',
-		});
-		await sdk.card.link(src1, src1final, 'was merged as');
+			type: 'service-source@1.0.0'
+		})
+		await sdk.card.link(src1, src1final, 'was merged as')
 
 		const src2test = await test.context.waitForMatch(
 			{
 				type: 'object',
-				required: ['id', 'data'],
+				required: [ 'id', 'data' ],
 				properties: {
 					id: {
-						const: src2.id,
+						const: src2.id
 					},
 					data: {
 						type: 'object',
-						required: ['$transformer'],
+						required: [ '$transformer' ],
 						properties: {
 							$transformer: {
 								type: 'object',
-								required: ['parentMerged'],
+								required: [ 'parentMerged' ],
 								properties: {
 									parentMerged: {
-										const: true,
-									},
-								},
-							},
-						},
-					},
-				},
+										const: true
+									}
+								}
+							}
+						}
+					}
+				}
 			},
-			4,
-		);
+			4
+		)
 
-		test.is(src2test.id, src2.id);
-	},
-);
+		test.is(src2test.id, src2.id)
+	}
+)
