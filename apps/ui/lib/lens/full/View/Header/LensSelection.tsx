e606eb9505fca63f715031a5df1ba05e497245b8
Lucian Buzzo <lucian.buzzo@gmail.com>
2021-11-01 15:59:59 +0000

    Correctly type View lens component
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- apps/ui/lib/lens/full/View/Header/LensSelection.tsx
+++ apps/ui/lib/lens/full/View/Header/LensSelection.tsx
@@ -6,15 +6,27 @@
 
 import React from 'react';
 import _ from 'lodash';
-import { Box, Button, ButtonGroup } from 'rendition';
+import { Box, BoxProps, Button, ButtonGroup } from 'rendition';
 import { Icon } from '@balena/jellyfish-ui-components';
+import { LensContract } from '../../../../types';
 
 // HACK: set min height to the height of a button group
 // this prevents the component collapsing vertically if
 // there are no lenses provided.
 const MIN_HEIGHT = '38px';
 
-export const LensSelection = ({ lenses, lens, setLens, ...rest }) => {
+interface LensSelectionProps extends BoxProps {
+	lenses: LensContract[];
+	lens: LensContract;
+	setLens: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const LensSelection = ({
+	lenses,
+	lens,
+	setLens,
+	...rest
+}: LensSelectionProps) => {
 	return (
 		<Box {...rest} minHeight={MIN_HEIGHT}>
 			{lenses.length > 1 && (
---
+++ apps/ui/lib/types.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) Balena.io - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+import { JSONSchema } from '@balena/jellyfish-types';
+import { Contract } from '@balena/jellyfish-types/build/core';
+
+// Utility type that allows you to change the return type of a function
+// From https://stackoverflow.com/a/50014868
+export type ReplaceReturnType<T extends (...a: any) => any, TNewReturn> = (
+	...a: Parameters<T>
+) => TNewReturn;
+
+// Utility type for typing bound action creator functions
+// It maps the call signature to the returh type of the redux thunk.
+// Without this, the return type would be the thunk itself.
+// Example:
+// interface ComponentProps {
+// 	actions: BoundActionCreators<Pick<typeof actionCreators, 'update' | 'create'>>;
+// }
+export type BoundActionCreators<
+	TActionCreators extends { [fn: string]: (...a: any) => any },
+> = {
+	[Property in keyof TActionCreators]: ReplaceReturnType<
+		TActionCreators[Property],
+		ReturnType<ReturnType<TActionCreators[Property]>>
+	>;
+};
+
+export interface LensContract
+	extends Pick<Contract, 'slug' | 'type' | 'version' | 'name' | 'data'> {
+	data: {
+		label: string;
+		pathRegExp?: string;
+		type: 'view' | '*';
+		supportsSlices?: boolean;
+		icon: string;
+		format: 'full' | 'summary' | 'snippet';
+		renderer: any;
+		filter: JSONSchema;
+	};
+}
+
+export interface ChannelContract
+	extends Pick<Contract, 'slug' | 'type' | 'active' | 'data' | 'created_at'> {
+	data: {
+		canonical?: boolean;
+		target: 'string';
+		head?: Contract;
+		cardType?: string;
+	};
+}
