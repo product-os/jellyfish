7b1156d0a8eaa6a8c13c83ccf4135fb8c04c0a29
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-02-03 16:22:43 +0000

    UI: Always show the link tab for the next entity type in the loop
    When viewing a contract, if that contract is part of the loop structure,
    always show the link tab for the next type in the loop flow. E.g. if
    looking at a pattern, the link tab for improvements will always be shown
    
    Fixes https://github.com/product-os/jellyfish/issues/7853
    
    Change-type: minor
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- apps/ui/lib/lens/common/CustomQueryTab/index.tsx
+++ apps/ui/lib/lens/common/CustomQueryTab/index.tsx
@@ -27,6 +27,43 @@ export const CustomQueryTab = connect(
 )(InnerCustomQueryTab);
 
 const getCustomQueries = memoize((typeCard) => {
+	// TODO: Don't hardcode these queries for loop contracts
+	if (typeCard.slug === 'support-thread') {
+		return [
+			{
+				title: 'Patterns',
+				type: 'pattern',
+				link: 'has attached',
+			},
+		];
+	}
+	if (typeCard.slug === 'pattern') {
+		return [
+			{
+				title: 'Improvements',
+				type: 'improvement',
+				link: 'has attached',
+			},
+		];
+	}
+	if (typeCard.slug === 'improvement') {
+		return [
+			{
+				title: 'Milestones',
+				type: 'milestone',
+				link: 'has attached',
+			},
+		];
+	}
+	if (typeCard.slug === 'milestone') {
+		return [
+			{
+				title: 'Issues',
+				type: 'issue',
+				link: 'is attached to',
+			},
+		];
+	}
 	return _.filter(
 		_.get(typeCard, ['data', 'meta', 'relationships'], []),
 		'query',
