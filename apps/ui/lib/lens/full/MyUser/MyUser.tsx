a9cd1edcf2cf49aa77a6c5057ed821a4cb025d66
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-05-06 09:51:12 +0100

    UI: Show active owned issues, milestones and improvements on profile page
    Change-type: minor
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- apps/ui/lib/lens/full/MyUser/MyUser.tsx
+++ apps/ui/lib/lens/full/MyUser/MyUser.tsx
@@ -1,14 +1,14 @@
 import * as jsonpatch from 'fast-json-patch';
 import _ from 'lodash';
 import React from 'react';
-import { Flex, Txt, Tab } from 'rendition';
+import { Box, Card, Divider, Flex, Txt, Tab } from 'rendition';
 import * as timezones from '../../../services/timezones';
 import * as helpers from '../../../services/helpers';
 import TabbedContractLayout from '../../../layouts/TabbedContractLayout';
 import LiveCollection from '../../common/LiveCollection';
 import { BoundActionCreators, LensRendererProps } from '../../../types';
-import { TypeContract } from '@balena/jellyfish-types/build/core';
-import { actionCreators } from '../../../core';
+import { TypeContract, Contract } from '@balena/jellyfish-types/build/core';
+import { actionCreators, sdk } from '../../../core';
 import { JsonSchema } from '@balena/jellyfish-types';
 
 export type OwnProps = LensRendererProps;
@@ -36,6 +36,9 @@ interface State {
 	orgsQuery: JsonSchema;
 	mineQuery: JsonSchema;
 	fetchingIntegrationUrl: boolean;
+	improvements: Contract[];
+	milestones: Contract[];
+	issues: Contract[];
 }
 
 export default class MyUser extends React.Component<Props, State> {
@@ -165,7 +168,56 @@ export default class MyUser extends React.Component<Props, State> {
 			orgsQuery,
 			mineQuery,
 			fetchingIntegrationUrl: false,
+			improvements: [],
+			milestones: [],
+			issues: [],
 		};
+	}
+
+	async componentDidMount() {
+		const results = await sdk.query({
+			type: 'object',
+			properties: {
+				type: {
+					type: 'string',
+					enum: ['improvement@1.0.0', 'milestone@1.0.0', 'issue@1.0.0'],
+				},
+				data: {
+					type: 'object',
+					required: ['status'],
+					properties: {
+						status: {
+							type: 'string',
+							not: {
+								enum: ['proposed', 'denied-or-failed', 'completed', 'closed'],
+							},
+						},
+					},
+				},
+			},
+			$$links: {
+				'is owned by': {
+					type: 'object',
+					required: ['id', 'type'],
+					properties: {
+						type: {
+							const: 'user@1.0.0',
+						},
+						id: {
+							const: this.props.card.id,
+						},
+					},
+				},
+			},
+		});
+
+		const groups = _.groupBy(results, 'type');
+
+		this.setState({
+			improvements: groups['improvement@1.0.0'] || [],
+			milestones: groups['milestone@1.0.0'] || [],
+			issues: groups['issue@1.0.0'] || [],
+		});
 	}
 
 	bindMethods(methods) {
@@ -228,6 +280,16 @@ export default class MyUser extends React.Component<Props, State> {
 
 	render() {
 		const { channel, card: user } = this.props;
+		const { improvements, milestones, issues } = this.state;
+
+		let snippetLens;
+
+		const resultSet = improvements.concat(milestones).concat(issues);
+		if (resultSet.length) {
+			const { getLenses } = require('../../');
+			const lenses = getLenses('snippet', resultSet[0], this.props.user);
+			snippetLens = lenses[0];
+		}
 
 		const firstName = _.get(user, ['data', 'profile', 'name', 'first']);
 		const lastName = _.get(user, ['data', 'profile', 'name', 'last']);
@@ -240,6 +302,7 @@ export default class MyUser extends React.Component<Props, State> {
 				card={user}
 				channel={channel}
 				title={<Txt bold>{name}</Txt>}
+				maxWidth="100%"
 				tabs={[
 					<Tab title="Loops" key="loops">
 						<Flex
@@ -310,7 +373,60 @@ export default class MyUser extends React.Component<Props, State> {
 						</Flex>
 					</Tab>,
 				]}
-			></TabbedContractLayout>
+			>
+				<Flex my={3} flexWrap="wrap">
+					<Card p={3} m={3} flex="1 1 300px">
+						<Txt bold>Active Issues</Txt>
+
+						{Boolean(snippetLens) && (
+							<Box mx={-3}>
+								{issues.map((c) => {
+									return (
+										<snippetLens.data.renderer
+											card={c}
+											types={this.props.types}
+										/>
+									);
+								})}
+							</Box>
+						)}
+					</Card>
+
+					<Card p={3} m={3} flex="1 1 300px">
+						<Txt bold>Active Milestones</Txt>
+
+						{Boolean(snippetLens) && (
+							<Box mx={-3}>
+								{milestones.map((c) => {
+									return (
+										<snippetLens.data.renderer
+											card={c}
+											types={this.props.types}
+										/>
+									);
+								})}
+							</Box>
+						)}
+					</Card>
+
+					<Card p={3} m={3} flex="1 1 300px">
+						<Txt bold>Active Improvements</Txt>
+
+						{Boolean(snippetLens) && (
+							<Box mx={-3}>
+								{improvements.map((c) => {
+									return (
+										<snippetLens.data.renderer
+											card={c}
+											types={this.props.types}
+										/>
+									);
+								})}
+							</Box>
+						)}
+					</Card>
+				</Flex>
+			</TabbedContractLayout>
 		);
 	}
 }
