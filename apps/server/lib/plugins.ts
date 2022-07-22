fec3432352037071a27a8587938c027afe13aa00
Josh Bowling <josh@balena.io>
2022-07-22 14:18:12 +0900

    Temporarily remove plugin-incidents
    Removing plugin-incidents as adding it in caused heavy locking
    in the production database on deployment. Will be adding this
    plugin back in once the underlying cause has been fixed.
    
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- apps/server/lib/plugins.ts
+++ apps/server/lib/plugins.ts
@@ -3,7 +3,6 @@ import { discoursePlugin } from '@balena/jellyfish-plugin-discourse';
 import { flowdockPlugin } from '@balena/jellyfish-plugin-flowdock';
 import { frontPlugin } from '@balena/jellyfish-plugin-front';
 import { githubPlugin } from '@balena/jellyfish-plugin-github';
-import { incidentsPlugin } from '@balena/jellyfish-plugin-incidents';
 import { outreachPlugin } from '@balena/jellyfish-plugin-outreach';
 import { typeformPlugin } from '@balena/jellyfish-plugin-typeform';
 import { PluginDefinition } from '@balena/jellyfish-worker';
@@ -17,6 +16,5 @@ export function getPlugins(): PluginDefinition[] {
 		outreachPlugin(),
 		frontPlugin(),
 		balenaApiPlugin(),
-		incidentsPlugin(),
 	] as any;
 }
