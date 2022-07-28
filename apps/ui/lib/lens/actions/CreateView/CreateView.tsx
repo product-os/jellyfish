fb4cc7e4bda3a59f5268c5eb11383eafeef603e8
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-07-28 15:13:14 +0100

    UI: Improve rendering of 1-2-1 conversation links in sidebar
    Change-type: minor
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- apps/ui/lib/lens/actions/CreateView/CreateView.tsx
+++ apps/ui/lib/lens/actions/CreateView/CreateView.tsx
@@ -118,32 +118,26 @@ export default withSetup(
 				},
 			};
 
-			// Check if the view already exists, otherwise create it
-			try {
-				const existing = await sdk.card.get(view.slug);
-				this.handleDone(existing);
-			} catch (err) {
-				sdk.card
-					.create(view)
-					.catch((error) => {
-						notifications.addNotification('danger', error.message);
-					})
-					.then((card) => {
-						if (card) {
-							this.props.analytics.track('element.create', {
-								element: {
-									type: card.type,
-								},
-							});
-						}
-						this.handleDone(card || null);
-					})
-					.finally(() => {
-						this.setState({
-							submitting: false,
+			sdk.card
+				.create(view)
+				.catch((error) => {
+					notifications.addNotification('danger', error.message);
+				})
+				.then((card) => {
+					if (card) {
+						this.props.analytics.track('element.create', {
+							element: {
+								type: card.type,
+							},
 						});
+					}
+					this.handleDone(card || null);
+				})
+				.finally(() => {
+					this.setState({
+						submitting: false,
 					});
-			}
+				});
 		}
 
 		close() {
