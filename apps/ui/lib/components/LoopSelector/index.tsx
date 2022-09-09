b3caa921f66bb1c7b86003242909dbc0a666d712
Edwin Joassart <edwin@blck.coffee>
2022-09-09 17:56:16 +0200

    unified org/product/loop selector change-type: minor

--- apps/ui/lib/components/LoopSelector/index.tsx
+++ apps/ui/lib/components/LoopSelector/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import _ from 'lodash';
 import { useDispatch, useSelector } from 'react-redux';
-import type { Contract, LoopContract, OrgContract } from 'autumndb';
+import { useState } from 'react';
+import type { Contract, contracts, LoopContract, OrgContract } from 'autumndb';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import { actionCreators, selectors } from '../../store';
 import { useSetup } from '../SetupProvider';
-import Select, { SelectProps } from 'react-select';
+import Select, { SelectProps, components } from 'react-select';
+import { Icon } from '../../components';
 
 const StyledSelect = styled(Select)`
 	margin-left: 16px;
@@ -21,136 +23,74 @@ export const LoopSelector = React.memo(() => {
 	const loops = useSelector(selectors.getLoops());
 	const stateActiveLoop = useSelector(selectors.getActiveLoop());
 	const channels = useSelector(selectors.getChannels());
-	const [products, setProducts] = React.useState<Contract[]>([]);
-	const [activeOrg, setActiveOrg] = React.useState<OrgContract | null>(null);
-	const [activeLoop, setActiveLoop] = React.useState<LoopContract | null>(null);
-	const [activeProduct, setActiveProduct] = React.useState<Contract | null>(
-		null,
-	);
+	const [allLoopsAndProducts, setAllLoopsAndProducts] = React.useState<
+		Contract[]
+	>([]);
 
 	if (!user) {
 		throw new Error('Cannot render without a user');
 	}
 
-	const orgs = user?.links?.['is member of'] ?? [];
+	const orgs =
+		user?.links?.['is member of'].map((org) => ({
+			label: org.name,
+			type: 'org',
+			value: org.slug,
+			icon: 'building',
+		})) ?? [];
 
 	React.useEffect(() => {
-		for (const channel of channels) {
-			for (const org of orgs) {
-				if (
-					org.id === channel.data.target ||
-					org.slug === channel.data.target
-				) {
-					return setActiveOrg(org);
-				}
-			}
-		}
-		setActiveOrg(null);
-	}, [channels]);
-
-	React.useEffect(() => {
-		for (const channel of channels) {
-			for (const loop of loops) {
-				if (
-					loop.id === channel.data.target ||
-					loop.slug === channel.data.target
-				) {
-					return setActiveLoop(loop);
-				}
-			}
-		}
-		setActiveLoop(null);
-		if (stateActiveLoop) {
-			dispatch(actionCreators.setActiveLoop(null));
-		}
-	}, [channels]);
-
-	React.useEffect(() => {
-		for (const channel of channels) {
-			for (const product of products) {
-				if (
-					product.id === channel.data.target ||
-					product.slug === channel.data.target
-				) {
-					if (!activeLoop) {
-						const linkedLoop = product?.links?.['is used by'][0]!;
-						if (
-							linkedLoop.id === channel.data.target ||
-							linkedLoop.slug === channel.data.target
-						) {
-							setActiveLoop(linkedLoop);
-						}
-					}
-					return setActiveProduct(product);
-				}
-			}
-		}
-		return setActiveProduct(null);
-	}, [channels]);
-
-	React.useEffect(() => {
-		if (activeLoop) {
-			sdk
-				.query({
-					type: 'object',
-					properties: {
-						type: {
-							const: 'repository@1.0.0',
-						},
+		sdk
+			.query({
+				type: 'object',
+				properties: {
+					type: {
+						const: 'loop@1.0.0',
 					},
-					$$links: {
-						'is used by': {
-							type: 'object',
-							properties: {
-								id: {
-									const: activeLoop.id,
-								},
+				},
+				$$links: {
+					has: {
+						type: 'object',
+						properties: {
+							type: {
+								const: 'repository@1.0.0',
 							},
 						},
 					},
-				})
-				.then((result) => {
-					setProducts(result);
-				})
-				.catch(console.error);
-		} else {
-			setProducts([]);
-		}
-	}, [activeLoop]);
-
-	const onOrgChange = React.useCallback(({ value }) => {
-		history.push(`/${value.slug}`);
+				},
+			})
+			.then((loopsAndProducts) => {
+				const landp = loopsAndProducts
+					.map((loop) => ({
+						label: loop.name,
+						value: loop.slug,
+						type: 'loop',
+						icon: 'circle',
+						products: loop?.links?.has
+							?.map((product) => ({
+								label: product.name,
+								value: product.slug,
+								icon: 'gem',
+							}))
+							.sort((productA: any, productB: any) =>
+								productA.label < productB.label ? -1 : 1,
+							),
+					}))
+					.reduce((aggregator: any, item) => {
+						return [...aggregator, [{ ...item }], item.products];
+					}, [])
+					.flat();
+				setAllLoopsAndProducts(landp);
+			})
+			.catch(console.error);
 	}, []);
 
-	const onLoopChange = React.useCallback(
-		({ value }) => {
-			if (value) {
-				if (activeOrg) {
-					history.push(`/${activeOrg.slug}/${value.slug}`);
-					dispatch(
-						actionCreators.setActiveLoop(`${value.slug}@${value.version}`),
-					);
-				}
-			} else {
-				history.push('/');
-				dispatch(actionCreators.setActiveLoop(null));
-			}
-		},
-		[activeOrg],
-	);
+	const onNavigationChange = React.useCallback(({ value, type }) => {
+		// TODO: set active loop ? Not sure if it's necessary
 
-	const onProductChange = React.useCallback(
-		({ value }) => {
-			if (activeLoop && activeOrg) {
-				if (value) {
-					history.push(`/${activeOrg.slug}/${activeLoop.slug}/${value.slug}`);
-				} else {
-					history.push(`/${activeOrg.slug}/${activeLoop.slug}`);
-				}
-			}
-		},
-		[activeOrg, activeLoop],
-	);
+		// navigate
+		history.push(`/${value}`);
+	}, []);
 
 	const height = 28;
 	const customStyles: SelectProps['styles'] = {
@@ -186,6 +126,32 @@ export const LoopSelector = React.memo(() => {
 		}),
 	};
 
+	const selectOptionStyleFor = (type) => {
+		switch (type) {
+			case 'loop':
+				return {
+					fontSize: '1.1em',
+					fontWeight: 'bold',
+				};
+			case 'org':
+				return {
+					fontSize: '1.3em',
+					fontWeight: 'bold',
+				};
+			case 'product':
+				return {};
+		}
+	};
+
+	const selectOption = (props) => (
+		<components.Option {...props}>
+			<span style={selectOptionStyleFor(props.data.type)}>
+				<Icon name={props.data.icon} regular style={{ float: 'right' }} />{' '}
+				<span>{props.data.label}</span>
+			</span>
+		</components.Option>
+	);
+
 	return (
 		<>
 			<StyledSelect
@@ -195,64 +161,10 @@ export const LoopSelector = React.memo(() => {
 				className="nav-selector"
 				id="orgselector__select"
 				blurInputOnSelect
-				placeholder="Select org..."
-				value={
-					activeOrg && {
-						value: activeOrg,
-						label: activeOrg.name,
-					}
-				}
-				options={orgs.map((option) => ({
-					value: option,
-					label: option.name,
-				}))}
-				onChange={onOrgChange}
-			/>
-			<StyledSelect
-				styles={customStyles}
-				width="180px"
-				ml={3}
-				className="nav-selector"
-				id="loopselector__select"
-				blurInputOnSelect
-				placeholder="Select loop..."
-				value={
-					activeLoop && {
-						value: activeLoop,
-						label: activeLoop.name,
-					}
-				}
-				options={
-					activeOrg
-						? loops.map((option) => ({
-								value: option,
-								label: option.name,
-						  }))
-						: []
-				}
-				onChange={onLoopChange}
-			/>
-			<StyledSelect
-				styles={customStyles}
-				width="180px"
-				ml={2}
-				className="nav-selector"
-				id="productselector__select"
-				placeholder="Select product..."
-				blurInputOnSelect
-				value={
-					activeProduct && {
-						value: activeProduct,
-						label: activeProduct.name,
-					}
-				}
-				options={products.map((option) => ({
-					value: option,
-					label: option.name,
-				}))}
-				labelKey="name"
-				valueKey="slug"
-				onChange={onProductChange}
+				placeholder="Navigate..."
+				components={{ Option: selectOption }}
+				options={[...orgs, ...allLoopsAndProducts]}
+				onChange={onNavigationChange}
 			/>
 		</>
 	);
