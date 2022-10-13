501fc66c3044b4bba2b0f1040b0cd5a299767b3e
Josh Bowling <45343541+joshbwlng@users.noreply.github.com>
2022-10-13 11:38:43 +0900

    Revert "Replace plugin-typeform with plugin-feedback"
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@monarci.com>

--- apps/server/lib/plugins.ts
+++ apps/server/lib/plugins.ts
@@ -1,15 +1,15 @@
 import { balenaApiPlugin } from '@balena/jellyfish-plugin-balena-api';
 import { discoursePlugin } from '@balena/jellyfish-plugin-discourse';
-import { feedbackPlugin } from '@balena/jellyfish-plugin-feedback';
 import { frontPlugin } from '@balena/jellyfish-plugin-front';
 import { githubPlugin } from '@balena/jellyfish-plugin-github';
 import { incidentsPlugin } from '@balena/jellyfish-plugin-incidents';
 import { outreachPlugin } from '@balena/jellyfish-plugin-outreach';
+import { typeformPlugin } from '@balena/jellyfish-plugin-typeform';
 import { PluginDefinition } from '@balena/jellyfish-worker';
 
 export function getPlugins(): PluginDefinition[] {
 	return [
-		feedbackPlugin(),
+		typeformPlugin(),
 		githubPlugin(),
 		discoursePlugin(),
 		outreachPlugin(),
