edd991dc81b8b40816a80596b78177372214ef91
Josh Bowling <josh@balena.io>
2023-02-07 11:11:58 +0900

    Remove jellyfish-plugin-incidents
    Change-type: major

--- apps/server/lib/plugins.ts
+++ apps/server/lib/plugins.ts
@@ -2,7 +2,6 @@ import { balenaApiPlugin } from '@balena/jellyfish-plugin-balena-api';
 import { discoursePlugin } from '@balena/jellyfish-plugin-discourse';
 import { frontPlugin } from '@balena/jellyfish-plugin-front';
 import { githubPlugin } from '@balena/jellyfish-plugin-github';
-import { incidentsPlugin } from '@balena/jellyfish-plugin-incidents';
 import { outreachPlugin } from '@balena/jellyfish-plugin-outreach';
 import { typeformPlugin } from '@balena/jellyfish-plugin-typeform';
 import { PluginDefinition } from '@balena/jellyfish-worker';
@@ -15,6 +14,5 @@ export function getPlugins(): PluginDefinition[] {
 		outreachPlugin(),
 		frontPlugin(),
 		balenaApiPlugin(),
-		incidentsPlugin(),
 	] as any;
 }
