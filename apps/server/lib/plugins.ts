e8d87c03130910871839354dab0820ef7592cb7e
Josh Bowling <josh@monarci.com>
2023-04-01 13:01:32 +0900

    Remove jellyfish-plugin-typeform
    Change-type: major

--- apps/server/lib/plugins.ts
+++ apps/server/lib/plugins.ts
@@ -2,12 +2,10 @@ import { balenaApiPlugin } from '@balena/jellyfish-plugin-balena-api';
 import { discoursePlugin } from '@balena/jellyfish-plugin-discourse';
 import { frontPlugin } from '@balena/jellyfish-plugin-front';
 import { githubPlugin } from '@balena/jellyfish-plugin-github';
-import { typeformPlugin } from '@balena/jellyfish-plugin-typeform';
 import { PluginDefinition } from '@balena/jellyfish-worker';
 
 export function getPlugins(): PluginDefinition[] {
 	return [
-		typeformPlugin(),
 		githubPlugin(),
 		discoursePlugin(),
 		frontPlugin(),
